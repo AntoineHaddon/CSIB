f8d3795bd82dfdbd152f8a4f9ce71e2afe5916e1
Duo Yang <Duo.Yang@ec.gc.ca>
2015-09-02 18:28:25 -0400

    cmip6 request - usage of iodef_ar6.xml and addition of 3D active tracer tendencies.
      - revision to trdtra.F90:
        Mainly added two subroutins (trd_tra_mng and trd_tra_iom) that were extracted
        from nemo_v3.6_stable and adapted to conform nemo_v3.4.1 naming convention, etc.

--- nemo/NEMO/OPA_SRC/TRD/trdtra.F90
+++ nemo/NEMO/OPA_SRC/TRD/trdtra.F90
@@ -6,18 +6,24 @@ MODULE trdtra
    !! History :  1.0  !  2004-08  (C. Talandier) Original code
    !!            2.0  !  2005-04  (C. Deltel)    Add Asselin trend in the ML budget
    !!            3.3  !  2010-06  (C. Ethe) merge TRA-TRC 
+   !!            3.4.1!  2015-08  (D. Yang) output 3D tracer trends
    !!----------------------------------------------------------------------
 #if  defined key_trdtra || defined key_trdtrc || defined key_trdmld || defined key_trdmld_trc 
    !!----------------------------------------------------------------------
    !!   trd_tra      : Call the trend to be computed
+   !!   trd_tra_adv   : transform a div(U.T) trend into a U.grad(T) trend
+   !!   trd_tra_mng   : tracer trend manager: dispatch to the diagnostic modules
+   !!   trd_tra_iom   : output 3D tracer trends using IOM
    !!----------------------------------------------------------------------
+   USE oce              ! ocean dynamics and tracers variables
    USE dom_oce          ! ocean domain 
    USE trdmod_oce       ! ocean active mixed layer tracers trends 
    USE trdmod           ! ocean active mixed layer tracers trends 
    USE trdmod_trc       ! ocean passive mixed layer tracers trends 
    USE in_out_manager   ! I/O manager
+   USE iom              ! I/O manager library
    USE lib_mpp          ! MPP library
-   USE wrk_nemo        ! Memory allocation
+   USE wrk_nemo         ! Memory allocation
 
 
    IMPLICIT NONE
@@ -71,7 +77,7 @@ CONTAINS
       REAL(wp), DIMENSION(jpi,jpj,jpk), INTENT(in), OPTIONAL ::  pun     ! velocity 
       REAL(wp), DIMENSION(jpi,jpj,jpk), INTENT(in), OPTIONAL ::  ptra    ! Tracer variablea
       !
-      REAL(wp), POINTER, DIMENSION(:,:,:)  ::  ztrds
+      REAL(wp), POINTER, DIMENSION(:,:,:)  ::  ztrds                     ! 3D workspace
       !!----------------------------------------------------------------------
 
       CALL wrk_alloc( jpi, jpj, jpk, ztrds )
@@ -81,39 +87,47 @@ CONTAINS
       ENDIF
       
       ! Control of optional arguments
-      IF( ctype == 'TRA' .AND. ktra == jp_tem ) THEN 
-         IF( PRESENT( ptra ) ) THEN    
+      IF( ctype == 'TRA' .AND. ktra == jp_tem ) THEN   !==  Temperature trend  ==!
+      !   IF( PRESENT( ptra ) ) THEN    
             SELECT CASE( ktrd )            ! shift depending on the direction
             CASE( jptra_trd_xad )  ;  CALL trd_tra_adv( ptrd, pun, ptra, 'X', trdtx ) 
             CASE( jptra_trd_yad )  ;  CALL trd_tra_adv( ptrd, pun, ptra, 'Y', trdty ) 
             CASE( jptra_trd_zad )  ;  CALL trd_tra_adv( ptrd, pun, ptra, 'Z', trdt  ) 
+            CASE( jptra_trd_bbc,   &       ! qsr, bbc: on temperature only, send to trd_tra_mng
+               &  jptra_trd_qsr )  ;  trdt(:,:,:) = ptrd(:,:,:) 
+                                      ztrds(:,:,:) = 0.
+                                      CALL trd_mod( trdt, ztrds, ktrd, ctype, kt )
+            CASE DEFAULT
+               trdt(:,:,:) = ptrd(:,:,:)
             END SELECT
-         ELSE
-            trdt(:,:,:) = ptrd(:,:,:)
-            IF( ktrd == jptra_trd_bbc .OR. ktrd == jptra_trd_qsr ) THEN
-               ztrds(:,:,:) = 0.
-               CALL trd_mod( trdt, ztrds, ktrd, ctype, kt )
-            END IF
-         END IF
+      !   ELSE
+      !   END IF
       END IF
 
       IF( ctype == 'TRA' .AND. ktra == jp_sal ) THEN 
-         IF( PRESENT( ptra ) ) THEN    
+      !   IF( PRESENT( ptra ) ) THEN    
             SELECT CASE( ktrd )            ! shift depending on the direction
             CASE( jptra_trd_xad )  
                                 CALL trd_tra_adv( ptrd, pun, ptra, 'X', ztrds ) 
+                                CALL trd_tra_mng( trdtx, ztrds, ktrd, kt   )
                                 CALL trd_mod( trdtx, ztrds, ktrd, ctype, kt   )
             CASE( jptra_trd_yad )  
                                 CALL trd_tra_adv( ptrd, pun, ptra, 'Y', ztrds ) 
+                                CALL trd_tra_mng( trdty, ztrds, ktrd, kt   )
                                 CALL trd_mod( trdty, ztrds, ktrd, ctype, kt   )
             CASE( jptra_trd_zad )    
                                 CALL trd_tra_adv( ptrd, pun, ptra, 'Z', ztrds ) 
+                                CALL trd_tra_mng( trdt , ztrds, ktrd, kt   )
                                 CALL trd_mod( trdt , ztrds, ktrd, ctype, kt   )
-            END SELECT
-         ELSE
+            CASE DEFAULT                 ! other trends: mask and send T & S trends to trd_tra_mng
+       !  ELSE
             ztrds(:,:,:) = ptrd(:,:,:)
+            !IF( ktrd == jptra_trd_ldf .OR. ktrd == jptra_trd_zdf ) THEN
+              CALL trd_tra_mng( trdt , ztrds, ktrd, kt   )
+            !END IF
             CALL trd_mod( trdt, ztrds, ktrd, ctype, kt )  
-         END IF
+            END SELECT
+       !  END IF
       END IF
 
       IF( ctype == 'TRC' ) THEN
@@ -186,6 +200,127 @@ CONTAINS
       END DO
       !
    END SUBROUTINE trd_tra_adv
+
+   SUBROUTINE trd_tra_mng( ptrdx, ptrdy, ktrd, kt )
+      !!---------------------------------------------------------------------
+      !!                  ***  ROUTINE trd_tra_mng  ***
+      !!
+      !! ** Purpose :   Dispatch all tracer trends computation, e.g. 3D output,
+      !!                integral constraints, potential energy, and/or
+      !!                mixed layer budget.
+      !!----------------------------------------------------------------------
+      REAL(wp), DIMENSION(:,:,:), INTENT(inout) ::   ptrdx   ! Temperature or U trend
+      REAL(wp), DIMENSION(:,:,:), INTENT(inout) ::   ptrdy   ! Salinity    or V trend
+      INTEGER                   , INTENT(in   ) ::   ktrd    ! tracer trend index
+      INTEGER                   , INTENT(in   ) ::   kt      ! time step
+      !!----------------------------------------------------------------------
+
+      !Duo IF( neuler == 0 .AND. kt == nit000    ) THEN   ;   r2dt =      rdt      ! = rdtra (restart with Euler time stepping)
+      !Duo ELSEIF(               kt <= nit000 + 1) THEN   ;   r2dt = 2. * rdt      ! = 2 rdttra (leapfrog)
+      !Duo ENDIF
+
+      !                   ! 3D output of tracers trends using IOM interface
+      IF( l_trdtra )          CALL trd_tra_iom ( ptrdx, ptrdy, ktrd, kt )
+
+      !                   ! Integral Constraints Properties for tracers trends                                       !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+      !Duo IF( ln_glo_trd )   CALL trd_glo( ptrdx, ptrdy, ktrd, 'TRA', kt )
+
+      !                   ! Potential ENergy trends
+      !Duo IF( ln_PE_trd  )   CALL trd_pen( ptrdx, ptrdy, ktrd, kt, r2dt )
+
+      !                   ! Mixed layer trends for active tracers
+      !Duo IF( ln_tra_mxl )   THEN
+         !-----------------------------------------------------------------------------------------------
+         ! W.A.R.N.I.N.G :
+         ! jptra_ldf : called by traldf.F90
+         !                 at this stage we store:
+         !                  - the lateral geopotential diffusion (here, lateral = horizontal)
+         !                  - and the iso-neutral diffusion if activated
+         ! jptra_zdf : called by trazdf.F90
+         !                 * in case of iso-neutral diffusion we store the vertical diffusion component in the
+         !                   lateral trend including the K_z contrib, which will be removed later (see trd_mxl)
+         !-----------------------------------------------------------------------------------------------
+
+      !Duo   SELECT CASE ( ktrd )
+      !Duo   CASE ( jptra_xad )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_xad, '3D' )   ! zonal    advection
+      !Duo   CASE ( jptra_yad )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_yad, '3D' )   ! merid.   advection
+      !Duo   CASE ( jptra_zad )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_zad, '3D' )   ! vertical advection
+      !Duo   CASE ( jptra_ldf )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_ldf, '3D' )   ! lateral  diffusion
+      !Duo   CASE ( jptra_bbl )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_bbl, '3D' )   ! bottom boundary layer
+      !Duo   CASE ( jptra_zdf )
+      !Duo      IF( ln_traldf_iso ) THEN ; CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_ldf, '3D' )   ! lateral  diffusion (K_z)
+      !Duo      ELSE                   ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_zdf, '3D' )   ! vertical diffusion (K_z)
+      !Duo      ENDIF
+      !Duo   CASE ( jptra_dmp )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_dmp, '3D' )   ! internal 3D restoring (tradmp)
+      !Duo   CASE ( jptra_qsr )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_for, '3D' )   ! air-sea : penetrative sol radiat
+      !Duo   CASE ( jptra_nsr )        ;   ptrdx(:,:,2:jpk) = 0._wp   ;   ptrdy(:,:,2:jpk) = 0._wp
+      !Duo                                 CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_for, '2D' )   ! air-sea : non penetr sol radiation
+      !Duo   CASE ( jptra_bbc )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_bbc, '3D' )   ! bottom bound cond (geoth flux)
+      !Duo   CASE ( jptra_npc )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_npc, '3D' )   ! non penetr convect adjustment
+      !Duo   CASE ( jptra_atf )        ;   CALL trd_mxl_zint( ptrdx, ptrdy, jpmxl_atf, '3D' )   ! asselin time filter (last trend)
+                                   !
+      !Duo                                 CALL trd_mxl( kt, r2dt )                             ! trends: Mixed-layer (output)
+      !Duo   END SELECT
+         !
+      !Duo ENDIF
+      !
+   END SUBROUTINE trd_tra_mng
+      
+
+   SUBROUTINE trd_tra_iom( ptrdx, ptrdy, ktrd, kt )
+      !!---------------------------------------------------------------------
+      !!                  ***  ROUTINE trd_tra_iom  ***
+      !!
+      !! ** Purpose :   output 3D tracer trends using IOM
+      !!----------------------------------------------------------------------
+      REAL(wp), DIMENSION(:,:,:), INTENT(inout) ::   ptrdx   ! Temperature or U trend
+      REAL(wp), DIMENSION(:,:,:), INTENT(inout) ::   ptrdy   ! Salinity    or V trend
+      INTEGER                   , INTENT(in   ) ::   ktrd    ! tracer trend index
+      INTEGER                   , INTENT(in   ) ::   kt      ! time step
+      !!
+      INTEGER ::   ji, jj, jk   ! dummy loop indices
+      INTEGER ::   ikbu, ikbv   ! local integers
+      REAL(wp), POINTER, DIMENSION(:,:)   ::   z2dx, z2dy   ! 2D workspace
+      !!----------------------------------------------------------------------
+      !
+!!gm Rq: mask the trends already masked in trd_tra, but lbc_lnk should probably be added
+      !
+      SELECT CASE( ktrd )
+      CASE( jptra_trd_xad  )   ;   CALL iom_put( "ttrd_xad" , ptrdx )        ! x- horizontal advection
+                                   CALL iom_put( "strd_xad" , ptrdy )
+      CASE( jptra_trd_yad  )   ;   CALL iom_put( "ttrd_yad" , ptrdx )        ! y- horizontal advection
+                                   CALL iom_put( "strd_yad" , ptrdy )
+      CASE( jptra_trd_zad  )   ;   CALL iom_put( "ttrd_zad" , ptrdx )        ! z- vertical   advection
+                                   CALL iom_put( "strd_zad" , ptrdy )
+                               IF( .NOT. lk_vvl ) THEN                   ! cst volume : adv flux through z=0 surface
+                                 CALL wrk_alloc( jpi, jpj, z2dx, z2dy )
+                                  z2dx(:,:) = wn(:,:,1) * tsn(:,:,1,jp_tem) / fse3t(:,:,1)
+                                  z2dy(:,:) = wn(:,:,1) * tsn(:,:,1,jp_sal) / fse3t(:,:,1)
+                                  CALL iom_put( "ttrd_sad", z2dx )
+                                  CALL iom_put( "strd_sad", z2dy )
+                                  CALL wrk_dealloc( jpi, jpj, z2dx, z2dy )
+                               ENDIF
+      CASE( jptra_trd_ldf  )   ;   CALL iom_put( "ttrd_ldf" , ptrdx )        ! lateral diffusion
+                                   CALL iom_put( "strd_ldf" , ptrdy )
+      CASE( jptra_trd_zdf  )   ;   CALL iom_put( "ttrd_zdf" , ptrdx )        ! vertical diffusion (including Kz contribution)
+                                   CALL iom_put( "strd_zdf" , ptrdy )
+      !CASE( jptra_trd_zdfp )   ;   CALL iom_put( "ttrd_zdfp", ptrdx )        ! PURE vertical diffusion (no isoneutral contribution)
+      !                             CALL iom_put( "strd_zdfp", ptrdy )
+      CASE( jptra_trd_dmp  )   ;   CALL iom_put( "ttrd_dmp" , ptrdx )        ! internal restoring (damping)
+                                   CALL iom_put( "strd_dmp" , ptrdy )
+      CASE( jptra_trd_bbl  )   ;   CALL iom_put( "ttrd_bbl" , ptrdx )        ! bottom boundary layer
+                                   CALL iom_put( "strd_bbl" , ptrdy )
+      CASE( jptra_trd_npc  )   ;   CALL iom_put( "ttrd_npc" , ptrdx )        ! static instability mixing
+                                   CALL iom_put( "strd_npc" , ptrdy )
+      CASE( jptra_trd_nsr  )   ;   CALL iom_put( "ttrd_qns" , ptrdx )        ! surface forcing + runoff (ln_rnf=T)
+                                   CALL iom_put( "strd_cdt" , ptrdy )
+      CASE( jptra_trd_qsr  )   ;   CALL iom_put( "ttrd_qsr" , ptrdx )        ! penetrative solar radiat. (only on temperature)
+      CASE( jptra_trd_bbc  )   ;   CALL iom_put( "ttrd_bbc" , ptrdx )        ! geothermal heating   (only on temperature)
+      CASE( jptra_trd_atf  )   ;   CALL iom_put( "ttrd_atf" , ptrdx )        ! asselin time Filter
+                                   CALL iom_put( "strd_atf" , ptrdy )
+      END SELECT
+      !
+   END SUBROUTINE trd_tra_iom
 
 #   else
    !!----------------------------------------------------------------------
