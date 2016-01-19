709549154467c240fb0f6f0b01343668c50c5e65
Larry Solheim <Larry.Solheim@ec.gc.ca>
2016-01-19 18:57:34 -0500

    send coupler elasped time in seconds to the ocean each coupling interval

--- nemo/NEMO/OPA_SRC/SBC/sbccpl.F90
+++ nemo/NEMO/OPA_SRC/SBC/sbccpl.F90
@@ -716,6 +716,9 @@ CONTAINS
         !--- Receive cpl_time_string from the coupler
         !--- cpl_time_string is found in the com_cpl module
         call bcast_inter(cpl_time_string, cpl_master, "ocn")
+        call bcast_inter(cpl_time_secs, cpl_master, "ocn")
+        write(numout,*)"isec=",isec,"  cpl_time_secs=",cpl_time_secs
+        call flush(numout)
       endif
 #endif
 
