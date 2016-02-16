1462ff417a7ed3b5d172cff2b53a954cfc5756e9
Neil Swart <Neil.Swart@ec.gc.ca>
2016-02-16 14:02:35 -0500

    Cleanout of unused PISCES code

--- CONFIG/CCC_ORCA2_LIM_CMOC/MY_SRC/p4zbio.F90
+++ CONFIG/CCC_ORCA2_LIM_CMOC/MY_SRC/p4zbio.F90
@@ -58,24 +58,13 @@ CONTAINS
       !!---------------------------------------------------------------------
       !
       IF( nn_timing == 1 )  CALL timing_start('p4z_bio')
-      !
-      !     ASSIGN THE SHEAR RATE THAT IS USED FOR AGGREGATION
-      !     OF PHYTOPLANKTON AND DETRITUS
 
-      xdiss(:,:,:) = 1.
-
-      DO jk = 2, jpkm1
-         DO jj = 1, jpj
-            DO ji = 1, jpi
-               IF( fsdepw(ji,jj,jk+1) > hmld(ji,jj) )   xdiss(ji,jj,jk) = 0.01
-            END DO 
-         END DO
-      END DO
-          
       CALL p4z_sink ( kt, jnt )     ! vertical flux of particulate organic matter
       CALL p4z_prod ( kt, jnt )     ! phytoplankton growth rate over the global ocean. 
       !                             ! (for each element : C, Si, Fe, Chl )
-      ! <CMOC code OR 10/19/2015) iom_put (in p4z_rem) must be called only once (over the physics time step), so jnt is to be passed to p4z_rem when saving Nfix if the diagnostics to be made on a complete physics time step.
+      ! <CMOC code OR 10/19/2015) iom_put (in p4z_rem) must be called only once (over the physics time step), 
+      ! so jnt is to be passed to p4z_rem when saving Nfix if the diagnostics to be made on a complete 
+      ! physics time step.
       CALL p4z_rem  ( kt, jnt )     
       CALL p4z_mort ( kt      )     ! phytoplankton mortality
       !                             ! zooplankton sources/sinks routines 
