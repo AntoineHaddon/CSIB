14ec130f4f9a30e641b60e7ad5dce4c64efc69d5
Larry Solheim <Larry.Solheim@ec.gc.ca>
2016-04-05 19:05:34 -0400

    allocate frcv(jpr_itx1:ity1)%z3 prior to these arrays being assigned in sbc_cpl_ice_tau

--- nemo/NEMO/OPA_SRC/SBC/sbccpl.F90
+++ nemo/NEMO/OPA_SRC/SBC/sbccpl.F90
@@ -974,14 +974,6 @@ call flush(6)
          IF( srcv(jpr_itx1)%laction ) THEN                      !   ice stress received   !
             !                                                   ! ======================= !
             !  
-
-            !LPS
-            !--- These arrays will not be allocated in some cases (e.g. when sn_rcv_tau%cldes /= 'oce and ice')
-            !--- Assume that the third dim is always 1 in this case since that is what gets assigned below
-            !--- This assumption is completely untested.
-            if ( .not. associated(frcv(jpr_itx1)%z3) ) allocate( frcv(jpr_itx1)%z3(jpi,jpj,1) )
-            if ( .not. associated(frcv(jpr_ity1)%z3) ) allocate( frcv(jpr_ity1)%z3(jpi,jpj,1) )
-
             IF( TRIM( sn_rcv_tau%clvref ) == 'cartesian' ) THEN            ! 2 components on the sphere
                !                                                       ! (cartesian to spherical -> 3 to 2 components)
                CALL geo2oce(  frcv(jpr_itx1)%z3(:,:,1), frcv(jpr_ity1)%z3(:,:,1), frcv(jpr_itz1)%z3(:,:,1),   &
@@ -1012,10 +1004,20 @@ call flush(6)
             !                                                   ! ======================= !
          ELSE                                                   !     use ocean stress    !
             !                                                   ! ======================= !
+
+            !LPS
+            !--- These arrays will not be allocated when srcv(jpr_itx1)%laction \= True so allocate them here
+            !--- Assume that the third dim is always 1 since that is what gets assigned below
+            !--- This assumption is completely untested (apparently this branch is also untested).
+            if ( .not. associated(frcv(jpr_itx1)%z3) ) allocate( frcv(jpr_itx1)%z3(jpi,jpj,1) )
+            if ( .not. associated(frcv(jpr_ity1)%z3) ) allocate( frcv(jpr_ity1)%z3(jpi,jpj,1) )
+
             frcv(jpr_itx1)%z3(:,:,1) = frcv(jpr_otx1)%z3(:,:,1)
             frcv(jpr_ity1)%z3(:,:,1) = frcv(jpr_oty1)%z3(:,:,1)
 
 !LPSDBG
+write(6,*)"Branch 2: shape(frcv(jpr_itx1)%z3) = ",shape(frcv(jpr_itx1)%z3)
+write(6,*)"Branch 2: shape(frcv(jpr_ity1)%z3) = ",shape(frcv(jpr_ity1)%z3)
 write(6,*)"Branch 2: sum(frcv(jpr_itx1)%z3(:,:,1)) = ",sum(frcv(jpr_itx1)%z3(:,:,1))
 write(6,*)"Branch 2: sum(frcv(jpr_ity1)%z3(:,:,1)) = ",sum(frcv(jpr_ity1)%z3(:,:,1))
 call flush(6)
