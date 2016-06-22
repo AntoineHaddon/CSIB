6017fa2da8aea7a87fef0708defcd347fd9273a1
Neil Swart <Neil.Swart@ec.gc.ca>
2016-06-22 18:02:50 -0400

    Changes EUCALC100 output scaling: replace *1e3 with zrfact2

--- nemo/CONFIG/CCC_ORCA1_LIM_CMOC/MY_SRC/p4zsink.F90
+++ nemo/CONFIG/CCC_ORCA1_LIM_CMOC/MY_SRC/p4zsink.F90
@@ -222,7 +222,7 @@ CONTAINS
            IF( jnt == nrdttrc ) THEN
               CALL iom_put( "oomask", oomask(:,:))
               CALL iom_put( "EPC100", sinking(:,:,ik1) * zrfact2 * tmask(:,:,1) )
-              CALL iom_put( "EPCALC100", zfpon(:,:) * rfact2r ) !
+              CALL iom_put( "EPCALC100",    zfpon(:,:) * zrfact2 * tmask(:,:,1) ) !
               ! <CMOC code OR 12/11/2015> denitrification ! CALL iom_put( "BUPOC"  , wsbio3(:,:,11) /rday * zbpoc(:,:) * 1e+3_wp  )  ! POC burial flux
               ! <CMOC code OR 12/11/2015> denitrification ! CALL iom_put( "BUCALC" , zfpon(:,:) * 1e+3_wp * rfact2r * zbpon(:,:)  )  ! <CMOC code OR 12/11/2015> *rfact2r replaces /rfact2 ! PIC burial flux
            ENDIF
