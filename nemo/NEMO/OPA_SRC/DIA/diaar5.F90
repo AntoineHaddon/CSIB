55975aff2ab855c28d5d0db826aecc35cd7fc380
Duo Yang <Duo.Yang@canada.ca>
2016-07-18 15:40:00 -0400

    Added global area average of SST and SSS.
      - modified paths:
          nemo/NEMO/OPA_SRC/DIA/diaar5.F90
          nemo/CONFIG/CCC_ORCA1_LIM/EXP00/iodef.xml

--- nemo/NEMO/OPA_SRC/DIA/diaar5.F90
+++ nemo/NEMO/OPA_SRC/DIA/diaar5.F90
@@ -5,6 +5,7 @@ MODULE diaar5
    !!======================================================================
    !! History :  3.2  !  2009-11  (S. Masson)  Original code
    !!            3.3  !  2010-10  (C. Ethe, G. Madec) reorganisation of initialisation phase + merge TRC-TRA
+   !!            3.4.1!  2016-06  (D. Yang) added global area average of SST and SSS.
    !!----------------------------------------------------------------------
 #if defined key_diaar5   || defined key_esopa
    !!----------------------------------------------------------------------
@@ -93,7 +94,6 @@ CONTAINS
       
       CALL iom_put( 'voltot', zvol               )
       CALL iom_put( 'sshtot', zvolssh / area_tot )
-
       !                     
       ztsn(:,:,:,jp_tem) = tsn(:,:,:,jp_tem)                    ! thermosteric ssh
       ztsn(:,:,:,jp_sal) = sn0(:,:,:)
@@ -160,6 +160,25 @@ CONTAINS
       CALL iom_put( 'temptot', ztemp )
       CALL iom_put( 'saltot' , zsal  )
       !
+      !                                         ! Mean sst and sss
+      ztemp = 0._wp
+      zsal  = 0._wp
+      DO jj = 1, jpj
+         DO ji = 1, jpi
+            ztemp = ztemp + area(ji,jj) * tsn(ji,jj,1,jp_tem)
+            zsal  = zsal  + area(ji,jj) * tsn(ji,jj,1,jp_sal)
+         END DO
+      END DO
+      IF( lk_mpp ) THEN
+         CALL mpp_sum( ztemp )
+         CALL mpp_sum( zsal  )
+      END IF
+      !
+      ztemp = ztemp / area_tot
+      zsal  = zsal  / area_tot
+      CALL iom_put( 'ssttot', ztemp)                  ! global area mean sst 
+      CALL iom_put( 'ssstot', zsal )                  ! global area mean sss
+      !
       CALL wrk_dealloc( jpi , jpj              , zarea_ssh , zbotpres )
       CALL wrk_dealloc( jpi , jpj , jpk        , zrhd      , zrhop    )
       CALL wrk_dealloc( jpi , jpj , jpk , jpts , ztsn                 )
@@ -198,6 +217,7 @@ CONTAINS
          vol0        = vol0        + SUM( area (:,:) * tmask(:,:,jk) * fse3t_0(:,:,jk) )
          thick0(:,:) = thick0(:,:) +    tmask_i(:,:) * tmask(:,:,jk) * fse3t_0(:,:,jk)
       END DO
+      
       IF( lk_mpp )   CALL mpp_sum( vol0 )
       
       CALL iom_open ( 'data_1m_salinity_nomask', inum )
