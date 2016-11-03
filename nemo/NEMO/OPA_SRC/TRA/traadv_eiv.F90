a1428477761796b0ad7f559aaa832336c3b8f166
Duo Yang <Duo.Yang@canada.ca>
2016-11-03 13:29:03 -0400

    Added computations of opottemppmadvect (degC s-1) and osaltpmadvect (1e-3 s-1) from cmip6 request.
      -- modified paths:
           nemo/NEMO/OPA_SRC/TRA/traadv_eiv.F90
           nemo/NEMO/OPA_SRC/TRD/trdmod_oce.F90
           nemo/NEMO/OPA_SRC/TRD/trdtra.F90
           nemo/CONFIG/CCC_ORCA1_LIM/EXP00/iodef.xml

--- nemo/NEMO/OPA_SRC/TRA/traadv_eiv.F90
+++ nemo/NEMO/OPA_SRC/TRA/traadv_eiv.F90
@@ -6,6 +6,7 @@ MODULE traadv_eiv
    !! History :  1.0  !  2005-11 (G. Madec)  Original code, from traldf and zdf _iso
    !!            3.3  !  2010-05 (C. Ethe, G. Madec)  merge TRC-TRA 
    !!            3.4.1!  2016-08 (D. Yang)   Added bolus mass transports.
+   !!            3.4.1!  2016-10 (D. Yang)   Added advection tendencies (T & S) from GM scheme.
    !!----------------------------------------------------------------------
 #if defined key_traldf_eiv   ||   defined key_esopa
    !!----------------------------------------------------------------------
@@ -26,6 +27,9 @@ MODULE traadv_eiv
    USE phycst          ! physical constants
    USE lbclnk          ! ocean lateral boundary conditions (or mpp link)
    USE diaar5, ONLY:   lk_diaar5
+   USE trdmod_oce, ONLY: jptra_trd_eiv, l_trdtra, l_trdtrc ! active tracer trend index and tracers trend flags
+   USE trdtra          ! ocean tracers trends
+   USE par_oce, ONLY:  jpts ! Number of active tracers (=2, i.e. T & S )
 # endif  
    USE wrk_nemo        ! Memory Allocation
    USE timing          ! Timing
@@ -34,6 +38,8 @@ MODULE traadv_eiv
    PRIVATE
 
    PUBLIC   tra_adv_eiv   ! routine called by step.F90
+
+   LOGICAL  :: l_trd      ! flag to compute trends
 
    !! * Substitutions
 #  include "domzgr_substitute.h90"
@@ -65,6 +71,10 @@ CONTAINS
       !!
       !! ** Action  : - add to p.n the eiv component
       !!----------------------------------------------------------------------
+# if defined key_diaeiv
+      USE oce     , ONLY:   zwx => ua        , zwy => va          ! (ua,va) used as workspace
+# endif
+      !
       INTEGER                         , INTENT(in   ) ::   kt       ! ocean time-step index
       INTEGER                         , INTENT(in   ) ::   kit000   ! first time step index
       CHARACTER(len=3)                , INTENT(in   ) ::   cdtype   ! =TRA or TRC (tracer indicator)
@@ -76,10 +86,15 @@ CONTAINS
       REAL(wp) ::   zuwk, zuwk1, zuwi, zuwi1   ! local scalars
       REAL(wp) ::   zvwk, zvwk1, zvwj, zvwj1   !   -      -
 # if defined key_diaeiv 
+      INTEGER  ::   jn                         ! dummy loop indices
       REAL(wp) ::   zztmp                      ! local scalar
       REAL(wp), POINTER, DIMENSION(:,:,:) :: z3d  
+      REAL(wp), POINTER, DIMENSION(:,:,:) :: pun_eiv, pvn_eiv, pwn_eiv ! transports
+      REAL(wp), POINTER, DIMENSION(:,:,:) :: zwz
+      REAL(wp), POINTER, DIMENSION(:,:,:) :: ptrdx_eiv, ptrdy_eiv, ptrdz_eiv ! advective trends in i,j,k directions
+      REAL(wp), POINTER, DIMENSION(:,:,:) :: ptrdt_eiv, ptrds_eiv            ! i + j + k advective trends
 # endif  
-      REAL(wp), POINTER, DIMENSION(:,:) :: zu_eiv, zv_eiv, zw_eiv, z2d
+      REAL(wp), POINTER, DIMENSION(:,:)   :: zu_eiv, zv_eiv, zw_eiv, z2d
       !!----------------------------------------------------------------------
       !
       IF( nn_timing == 1 )  CALL timing_start( 'tra_adv_eiv')
@@ -87,6 +102,10 @@ CONTAINS
 # if defined key_diaeiv 
       CALL wrk_alloc( jpi, jpj, zu_eiv, zv_eiv, zw_eiv, z2d )
       CALL wrk_alloc( jpi, jpj, jpk, z3d )
+      IF( cdtype == 'TRA' .AND. lk_diaar5 ) THEN
+         CALL wrk_alloc( jpi, jpj, jpk, pun_eiv, pvn_eiv, pwn_eiv )
+         pun_eiv(:,:,:) = 0.e0 ; pvn_eiv(:,:,:) = 0.e0 ; pwn_eiv(:,:,:) = 0.e0
+      END IF
 # else
       CALL wrk_alloc( jpi, jpj, zu_eiv, zv_eiv, zw_eiv )
 # endif
@@ -95,6 +114,9 @@ CONTAINS
          IF(lwp) WRITE(numout,*)
          IF(lwp) WRITE(numout,*) 'tra_adv_eiv : eddy induced advection on ', cdtype,' :'
          IF(lwp) WRITE(numout,*) '~~~~~~~~~~~   add to velocity fields the eiv component'
+         !
+         l_trd = .FALSE.
+         IF( ( cdtype == 'TRA' .AND. l_trdtra ) .OR. ( cdtype == 'TRC' .AND. l_trdtrc ) ) l_trd = .TRUE.
 # if defined key_diaeiv 
          IF( cdtype == 'TRA') THEN
             u_eiv(:,:,:) = 0.e0
@@ -103,6 +125,12 @@ CONTAINS
          END IF
 # endif
       ENDIF
+
+# if defined key_diaeiv
+      IF( cdtype == 'TRA' .AND. lk_diaar5 .AND. l_trd )  THEN
+         CALL wrk_alloc( jpi, jpj, jpk, zwz, ptrdx_eiv, ptrdy_eiv, ptrdz_eiv, ptrdt_eiv, ptrds_eiv )
+      END IF
+# endif
 
       zu_eiv(:,:) = 0.e0   ;   zv_eiv(:,:) = 0.e0   ;    zw_eiv(:,:) = 0.e0  
       
@@ -166,20 +194,57 @@ CONTAINS
          IF( lk_diaar5 ) THEN
             z3d(:,:,jpk) = 0.e0 
             DO jk = 1, jpkm1
-               z3d(:,:,jk) = rau0 * u_eiv(:,:,jk) * e2u(:,:) * fse3u(:,:,jk) * umask(:,:,jk)
+               pun_eiv(:,:,jk) = u_eiv(:,:,jk) * fse3u(:,:,jk) * e2u(:,:)
+               z3d(:,:,jk) = rau0 * pun_eiv(:,:,jk) * umask(:,:,jk)
             END DO
             CALL iom_put( "ueiv_masstr", z3d )                  ! bolus mass transport in i-direction
             z3d(:,:,jpk) = 0.e0
             DO jk = 1, jpkm1
-               z3d(:,:,jk) = rau0 * v_eiv(:,:,jk) * e1v(:,:) * fse3v(:,:,jk) * vmask(:,:,jk)
+               pvn_eiv(:,:,jk) = v_eiv(:,:,jk) * fse3v(:,:,jk) * e1v(:,:)
+               z3d(:,:,jk) = rau0 * pvn_eiv(:,:,jk) * vmask(:,:,jk)
             END DO
             CALL iom_put( "veiv_masstr", z3d )                  ! bolus mass transport in j-direction 
-            z2d(:,:) = rau0 * e1t(:,:) * e2t(:,:)
             DO jk = 1, jpk
-               z3d(:,:,jk) = w_eiv(:,:,jk) * z2d(:,:)
+               pwn_eiv(:,:,jk) = w_eiv(:,:,jk) * e1t(:,:) * e2t(:,:)
+               z3d(:,:,jk) = rau0 * pwn_eiv(:,:,jk) 
             END DO
             CALL iom_put( "weiv_masstr" , z3d )                 ! bolus mass transport in k-direction
-            !
+            ! computations of opottemppmadvect & ocontemppmadvect (tendencies, degC/s & 1e-3 s-1)
+            IF( l_trd )  THEN 
+               ! initialization and bottom flux set to zero
+               zwx(:,:,:) = 0.e0 ; zwy(:,:,:) = 0.e0 ; zwz(:,:,:) = 0.e0
+               DO jn = 1, jpts
+               ! antidiffusive flux on i and j
+                  DO jk = 1, jpkm1
+                     DO jj = 1, jpjm1
+                        DO ji = 1, fs_jpim1   ! vector opt.
+                           zwx(ji,jj,jk) = 0.5 * pun_eiv(ji,jj,jk) * ( tsn(ji,jj,jk,jn) + tsn(ji+1,jj,jk,jn) )
+                           zwy(ji,jj,jk) = 0.5 * pvn_eiv(ji,jj,jk) * ( tsn(ji,jj,jk,jn) + tsn(ji,jj+1,jk,jn) )
+                        END DO
+                     END DO
+                  END DO
+                  ! antidiffusive flux on k
+                  zwz(:,:,1) = 0.e0         ! Surface value
+                  DO jk = 2, jpkm1          ! Interior value
+                     DO jj = 1, jpj
+                        DO ji = 1, jpi
+                           zwz(ji,jj,jk) = 0.5 * pwn_eiv(ji,jj,jk) * ( tsn(ji,jj,jk,jn) + tsn(ji,jj,jk-1,jn) )
+                        END DO
+                     END DO
+                  END DO
+                  CALL lbc_lnk( zwx, 'U', -1. )
+                  CALL lbc_lnk( zwy, 'V', -1. )         ! Lateral bondary conditions
+                  CALL lbc_lnk( zwz, 'W',  1. )
+                  !
+                  CALL trd_tra_adv( zwx, pun_eiv, tsn(:,:,:,jn), 'X', ptrdx_eiv )
+                  CALL trd_tra_adv( zwy, pvn_eiv, tsn(:,:,:,jn), 'Y', ptrdy_eiv )
+                  CALL trd_tra_adv( zwz, pwn_eiv, tsn(:,:,:,jn), 'Z', ptrdz_eiv )
+                  IF( jn == jp_tem ) ptrdt_eiv = ptrdx_eiv + ptrdy_eiv + ptrdz_eiv
+                  IF( jn == jp_sal ) ptrds_eiv = ptrdx_eiv + ptrdy_eiv + ptrdz_eiv
+               END DO
+               CALL trd_tra_mng( ptrdt_eiv, ptrds_eiv, jptra_trd_eiv, kt )
+            END IF
+            ! end of opottemppmadvect & ocontemppmadvect
             zztmp = 0.5 * rau0 * rcp 
             z2d(:,:) = 0.e0 
             DO jk = 1, jpkm1
@@ -204,12 +269,18 @@ CONTAINS
             CALL lbc_lnk( z2d, 'V', -1. )
             CALL iom_put( "veiv_heattr", z2d )                  !  heat transport in i-direction
          ENDIF
-    END IF
+      END IF
 # endif  
       ! 
 # if defined key_diaeiv 
       CALL wrk_dealloc( jpi, jpj, zu_eiv, zv_eiv, zw_eiv, z2d )
       CALL wrk_dealloc( jpi, jpj, jpk, z3d )
+      IF( cdtype == 'TRA' .AND. lk_diaar5 ) THEN
+         CALL wrk_dealloc( jpi, jpj, jpk, pun_eiv, pvn_eiv, pwn_eiv )
+         IF( l_trd )  THEN
+            CALL wrk_dealloc( jpi, jpj, jpk, zwz, ptrdx_eiv, ptrdy_eiv, ptrdz_eiv, ptrdt_eiv, ptrds_eiv )
+         END IF
+      END IF
 # else
       CALL wrk_dealloc( jpi, jpj, zu_eiv, zv_eiv, zw_eiv )
 # endif
