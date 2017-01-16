f2f04982ad324f83e829b938847d892d59f6e547
Jim Christian <acrnrjc@ib3-fe01x.cmc.ec.gc.ca>
2017-01-16 13:51:36 -0500

    moved mortality diagnostic arrays zmortpn and zmortpd to after reductions for threshold and C/N/Fe stoichiometry changed below threshold mortality to retain quadratic term and zero only the linear term

--- nemo/CONFIG/CCC_ORCA2_LIM_CANOE/MY_SRC/p4zmort.F90
+++ nemo/CONFIG/CCC_ORCA2_LIM_CANOE/MY_SRC/p4zmort.F90
@@ -108,11 +108,11 @@ CONTAINS
 
 ! simplified CMOC type mortality: sum of linear and quadratic terms
                zmortp = mprat * xstep * spc + mpqua * xstep * spc * spc
-               zmortpn(ji,jj,jk) = zmortp       ! I will leave this here for now because that's where Hakase put it, but I think it should be after the if
+               if (spc.le.xminp) mpqua * xstep * spc * spc           ! no linear mortality below biomass threshold xminp
                zmortz = mprat * xstep * szc + mpqua * xstep * szc * szc
 ! reduce mortality to what can support detritus production based on the least abundant element: the MIN(...) term should be 1 if N and Fe are in excess of the detritus ratio
                zmortp=zmortp*MIN(n2c*rr_c2n,fe2c*rr_c2fe,1.)
-               if (spc.le.xminp) zmortp=0.                                    ! this is how Tessa did it but I would prefer to limit it to linear mortality only
+               zmortpn(ji,jj,jk) = zmortp
 ! calculate "excess" relative to grazer RR
                cxs=zmortp*MAX(c2n*rr_n2c-1.,c2fe*rr_fe2c-1.,0.)
                nxs1=zmortp*(n2c-rr_n2c)
@@ -195,11 +195,11 @@ CONTAINS
                thetac=chl/(spc+rtrn)
 
                zmortp = mpratm * xstep * spc + mpqua * xstep * spc * spc
-               zmortpd(ji,jj,jk) = zmortp
+               if (spc.le.xminp) mpqua * xstep * spc * spc
                zmortz = mprat2 * xstep * szc + mpquad * xstep * szc * szc
 ! reduce mortality to what can support detritus production based on the least abundant element: the MIN(...) term should be 1 if N and Fe are in excess of the detritus ratio
                zmortp=zmortp*MIN(n2c*rr_c2n,fe2c*rr_c2fe,1.)
-               if (spc.le.xminp) zmortp=0.
+               zmortpd(ji,jj,jk) = zmortp
 ! calculate "excess" relative to grazer RR
                cxs=zmortp*MAX(c2n*rr_n2c-1.,c2fe*rr_fe2c-1.,0.)
                nxs1=zmortp*(n2c-rr_n2c)
