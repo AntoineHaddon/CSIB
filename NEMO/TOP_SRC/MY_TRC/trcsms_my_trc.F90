b8fb5fcec930e967a79d95e56d70a1fef3a10a79
Duo Yang <duo.yang@canada.ca>
2018-03-07 00:00:23 +0000

    Implementation of tracers o1 & o2 read from file(s)

--- NEMO/TOP_SRC/MY_TRC/trcsms_my_trc.F90
+++ NEMO/TOP_SRC/MY_TRC/trcsms_my_trc.F90
@@ -17,12 +17,18 @@ MODULE trcsms_my_trc
    USE trdmod_oce
    USE trdmod_trc
    USE phycst
+   USE fldread         ! read input fields
 
    IMPLICIT NONE
    PRIVATE
 
    PUBLIC   trc_sms_my_trc       ! called by trcsms.F90 module
    PUBLIC   trc_sms_my_trc_alloc ! called by trcini_my_trc.F90 module
+
+   INTEGER , PARAMETER ::   jpfld = 2   ! maximum number of files to read
+   INTEGER , PARAMETER ::   jpo1  = 1   ! index of tracer o1
+   INTEGER , PARAMETER ::   jpo2  = 2   ! index of tracer o2
+   TYPE(FLD), ALLOCATABLE, DIMENSION(:) ::   sf    ! structure of input fields (file informations, fields read)
 
    !! * Substitution
 #  include "domzgr_substitute.h90"
@@ -47,7 +53,12 @@ CONTAINS
       !
       INTEGER, INTENT(in) ::   kt   ! ocean time-step index
       INTEGER ::   jn   ! dummy loop index
+      REAL(wp) :: zfact = 1._wp             ! temporary scalar
       REAL(wp) :: dtyrs
+      CHARACTER(len=100) ::  cn_dir             ! Root directory for location of tracer files
+      TYPE(FLD_N), DIMENSION(jpfld) ::   slf_i  ! array of namelist information structures
+      TYPE(FLD_N) :: sn_o1, sn_o2               ! informations about the fields to be read
+      NAMELIST/namsbc_olo2/ cn_dir, ln_o1, ln_o2, sn_o1, sn_o2
 !!----------------------------------------------------------------------
       !
       IF( nn_timing == 1 )  CALL timing_start('trc_sms_my_trc')
@@ -68,7 +79,56 @@ CONTAINS
       ! WRITE(numout,*) 'Max surface, ocean age', maxval(trn(:,:,1,jpage)), maxval(trn(:,:,:,jpage))
       ! WRITE(numout,*) 'Max surface, ocean age tra:', maxval(tra(:,:,1,jpage)), maxval(tra(:,:,:,jpage))
 
-! Oleg special tracers
+! Special tracers o1 & o2
+      IF( kt == nit000 ) THEN                  ! First call kt=nit000
+      ! set file information
+         cn_dir = './'                         ! directory in which the model is executed
+         ! ... default values (NB: frequency positive => hours, negative => months)
+         !            !  file  ! frequency !  variable      ! time intep !  clim   ! 'yearly' or ! weights  ! rotation  !
+         !            !  name  ! (hours)   !   name         !   (T/F)    !  (T/F)  ! 'monthly'   ! filename ! pairs     !
+         sn_o1 = FLD_N(  'o1' ,    -1     , 'heat_flux_anom',   .false.  , .true. ,   'yearly'  ,  ''       , ''        )
+         sn_o2 = FLD_N(  'o2' ,    -1     , 'heat_flux_anom',   .false.  , .true. ,   'yearly'  ,  ''       , ''        )
+         !
+         REWIND ( numnam )                     ! read in namlist namsbc_o1o2
+         READ   ( numnam, namsbc_o1o2 )           
+         !                                     ! Control print
+         IF(lwp) THEN
+            WRITE(numout,*)
+            WRITE(numout,'trc_sms_my_trc : o1 o2 tracers'
+            WRITE(numout,*) '~~~~~~~ '
+            WRITE(numout,*) '   Namelist namsbc_o1o2'
+            WRITE(numout,*) 'o1 flux in a file to be read  ln_o1 = ', ln_o1
+            WRITE(numout,*) 'o2 flux in a file to be read  ln_o2 = ', ln_o2
+         ENDIF
+         !
+         !                                     ! store namelist information in an array
+         slf_i(jpo1) = sn_o1   ;   slf_i(jpo2) = sn_o2
+         !
+         ALLOCATE( sf(jpfld), STAT=ierror )    ! set sf structure
+         IF( ierror > 0 ) THEN
+            CALL ctl_stop( 'trc_sms_my_trc: unable to allocate sf structure' )   ;   RETURN
+         ENDIF
+         DO ji= 1, jpfld
+            ALLOCATE( sf(ji)%fnow(jpi,jpj,1) )
+            IF( slf_i(ji)%ln_tint ) ALLOCATE( sf(ji)%fdta(jpi,jpj,1,2) )
+         END DO
+         !
+         CALL fld_fill( sf, slf_i, cn_dir, 'trc_sms_my_trc', 'o1 o2 tracers', 'namsbc_o1o2' )
+         !
+      ENDIF
+      IF( ln_o1 .or. ln_o2 ) THEN
+         CALL fld_read( kt, nn_fsbc, sf )      ! input fields provided at the current time-step
+      !
+         IF( MOD( kt-1, nn_fsbc ) == 0 ) THEN  ! update o1 o2 fluxes at each SBC frequency
+            tra(:,:,1,jpo1) = zfact * sf(jpo1)%fnow(:,:,1) * ro0cpr / fse3t(:,:,1)
+            tra(:,:,1,jpo2) = zfact * sf(jpo2)%fnow(:,:,1) * ro0cpr / fse3t(:,:,1)
+         ENDIF
+      ELSE
+      ! Apply uniform fluxes of 1 W/m2 and 2 W/m2 to tracers.
+         tra(:,:,1,jpo1) = 1._wp * ro0cpr / fse3t(:,:,1)
+         tra(:,:,1,jpo2) = 2._wp * ro0cpr / fse3t(:,:,1) 
+      ENDIF
+  
       ! To get the heat flux anomaly field from netcdf, do something like for runoff:
       ! CALL fld_read ( kt, nn_fsbc, sf_rnf   )    ! Read Runoffs data and provide it at kt
       ! rnf(:,:) =  sf_rnf(1)%fnow(:,:,1) 
@@ -79,10 +139,6 @@ CONTAINS
       ! sbc_tsc(ji,jj,jp_tem) = ro0cpr * qns(ji,jj) ! where qns is our heat flux anomaly
       ! z1_e3t = zfact / fse3t(ji,jj,1)
       ! tsa(ji,jj,1,jn) = tsa(ji,jj,1,jn) + ( sbc_tsc_b(ji,jj,jn) + sbc_tsc(ji,jj,jn) ) * z1_e3t
-
-      ! Apply uniform fluxes of 1 W/m2 and 2 W/m2 to tracers.
-      tra(:,:,1,jpo1) = 1._wp * ro0cpr / fse3t(:,:,1)
-      tra(:,:,1,jpo2) = 2._wp * ro0cpr / fse3t(:,:,1) 
 
       IF( nn_timing == 1 )  CALL timing_stop('trc_sms_my_trc')
       !
