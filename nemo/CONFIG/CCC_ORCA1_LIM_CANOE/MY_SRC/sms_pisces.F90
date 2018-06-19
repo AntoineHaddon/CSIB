f5814c5212c64c1c027330899fbc934f1fd1014f
cccma ice <ice101@ib7-fe01.science.gc.ca>
2018-06-19 23:01:07 +0000

    added OMIP-BGC updates to carbon chemistry and gas exchange to CCC_ORCA1_LIM_CANOE 2018/06/19 JRC

--- nemo/CONFIG/CCC_ORCA1_LIM_CANOE/MY_SRC/sms_pisces.F90
+++ nemo/CONFIG/CCC_ORCA1_LIM_CANOE/MY_SRC/sms_pisces.F90
@@ -56,7 +56,7 @@ MODULE sms_pisces
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   grazing3   !: mesozooplankton grazing on microzooplankton
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   denitr     !: denitrification
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   zdnf       !: N2 fixation
-   REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   denitnh4   !: anammox
+   REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   denitnh4   !: annamox
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   nh4ox      !: nitrification
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   zmortpn    !: nanophytoplankton mortality 
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   zmortpd    !: diatoms mortality
@@ -69,6 +69,11 @@ MODULE sms_pisces
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   ak23       !: ???
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   aksp       !: ???
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   akw3       !: ???
+   REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   akp13
+   REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   akp23
+   REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   akp33
+   REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   aksi3
+   REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   asi3
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   borat      !: ???
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   hi         !: ???
    REAL(wp), ALLOCATABLE, SAVE, DIMENSION(:,:,:) ::   excess     !: ???
@@ -115,6 +120,9 @@ CONTAINS
       ALLOCATE( akb3(jpi,jpj,jpk)    , ak13  (jpi,jpj,jpk) ,       &
          &      ak23(jpi,jpj,jpk)    , aksp  (jpi,jpj,jpk) ,       &
          &      akw3(jpi,jpj,jpk)    , borat (jpi,jpj,jpk) ,       &
+         &      akp13(jpi,jpj,jpk)   , akp23 (jpi,jpj,jpk) ,       &
+         &      akp33(jpi,jpj,jpk)   , aksi3 (jpi,jpj,jpk) ,       &
+         &      asi3 (jpi,jpj,jpk)   ,                             &
          &      hi  (jpi,jpj,jpk)    , excess(jpi,jpj,jpk) ,   STAT=ierr(4) )
          !
       !* Temperature dependence of SMS terms
