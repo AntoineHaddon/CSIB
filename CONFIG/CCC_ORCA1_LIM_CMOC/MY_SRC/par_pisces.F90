b868784272df18b80e8f3a30fa15dc00de8d1477
Andrew Shao <andrew.shao@canada.ca>
2018-08-23 23:50:40 +0000

    Finalize fixes for parameterizing abiotic alkalinity

--- CONFIG/CCC_ORCA1_LIM_CMOC/MY_SRC/par_pisces.F90
+++ CONFIG/CCC_ORCA1_LIM_CMOC/MY_SRC/par_pisces.F90
@@ -28,7 +28,7 @@ MODULE par_pisces
    !!---------------------------------------------------------------------
    LOGICAL, PUBLIC, PARAMETER ::   lk_pisces     = .TRUE.  !: PISCES flag 
    LOGICAL, PUBLIC, PARAMETER ::   lk_kriest     = .FALSE. !: Kriest flag 
-   INTEGER, PUBLIC, PARAMETER ::   jp_pisces     = 13      !: CMOC1 tracers
+   INTEGER, PUBLIC, PARAMETER ::   jp_pisces     = 12      !: CMOC1 tracers
    INTEGER, PUBLIC, PARAMETER ::   jp_pisces_2d  = 13      !: additional 2d output 
    INTEGER, PUBLIC, PARAMETER ::   jp_pisces_3d  = 11      !: additional 3d output 
    INTEGER, PUBLIC, PARAMETER ::   jp_pisces_trd =  1      !: number of sms trends for PISCES
