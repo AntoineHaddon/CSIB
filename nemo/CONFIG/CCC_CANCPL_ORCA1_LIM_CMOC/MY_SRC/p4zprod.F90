e26d1da8011e1f593f76f57522f3e2b3b68d712a
Neil Swart <neil.swart@canada.ca>
2018-12-01 04:38:27 +0000

    NEMO bugfixes
    -removes accidentally committed key_cfc
    - adds PAR iom_put to p4zprod
    - removes alkabio from iodef

--- nemo/CONFIG/CCC_CANCPL_ORCA1_LIM_CMOC/MY_SRC/p4zprod.F90
+++ nemo/CONFIG/CCC_CANCPL_ORCA1_LIM_CMOC/MY_SRC/p4zprod.F90
@@ -194,7 +194,7 @@ CONTAINS
               CALL iom_put( "LNnut"   , zlimn   (:,:,:) * tmask_bgc_closea(:,:,:) )
               CALL iom_put( "LNFe"    , xlimnfecmoc (:,:) * tmask_bgc_closea(:,:,1) )
               CALL iom_put( "LNlight" , zliml   (:,:,:) * tmask_bgc_closea(:,:,:) )
-              
+              CALL iom_put( "PAR"   , zetot   (:,:,:) * tmask_bgc_closea(:,:,:) )
            ENDIF
            
           ENDIF
