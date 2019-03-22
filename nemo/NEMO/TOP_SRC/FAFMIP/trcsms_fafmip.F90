5f1c67850d2ba249f19efa3a34c0f9667a92dcce
Andrew Shao <andrew.shao@canada.ca>
2019-03-22 21:10:16 +0000

    Add a factor of 2 to passive heat tendency due when using BGC

--- nemo/NEMO/TOP_SRC/FAFMIP/trcsms_fafmip.F90
+++ nemo/NEMO/TOP_SRC/FAFMIP/trcsms_fafmip.F90
@@ -51,7 +51,7 @@ CONTAINS
       INTEGER ::   ji, jj, jn                       ! dummy loop index
       INTEGER  ::  ierror                       ! return error code
 !!----------------------------------------------------------------------
-      REAL(wp) :: zfact1, zfact2, z1_e3t
+      REAL(wp) :: zfact, z1_e3t
 !
       IF( nn_timing == 1 )  CALL timing_start('trc_sms_fafmip')
       !
@@ -63,33 +63,20 @@ CONTAINS
       ! and active tracers may be timestepped differently because PISCES assumes
       ! Euler timestepping (and thus need to multiple the tendency by 2)
 # if defined key_pisces
-     zfact1 = 1.
-     zfact2 = 2.
+     zfact = 2.
 # else
-      IF( kt == nit000 ) THEN                     ! Set the forcing field at nit000 - 1
-         !                                        ! -----------------------------------
-         IF( ln_rstart )                          ! Restart: read in restart file
-            zfact1 = 0.5e0
-            zfact2 = 0.5e0
-         ELSE
-            zfact1 = 1.e0
-            zfact1 = 1.e0
-         ENDIF
-      ELSE
-         zfact1 = 0.5e0
-         zfact2 = 0.5e0
-      ENDIF
+     zfact = 1.
 # endif
 
       ! Apply fluxes to redistributed heat tracer
-      tra(:,:,:,jpTr) = zfact2*Tr_sbc(:,:,:)  ! This is the heat flux from actual model components
+      tra(:,:,:,jpTr) = zfact*Tr_sbc(:,:,:)  ! This is the heat flux from actual model components
       ! Reset the sbc flux array to 0.
       Tr_sbc(:,:,:) = 0.
 
       ! Apply fluxes to added heat tracer
       DO jj = 1, jpj
          DO ji = 1, jpi
-            z1_e3t = zfact1 / fse3t(ji,jj,1)
+            z1_e3t = zfact / fse3t(ji,jj,1)
             tra(ji,jj,1,jpTa) = (sf_fafmip(jp_hflx)%fnow(ji,jj,1) * ro0cpr) * z1_e3t
          ENDDO
       ENDDO
