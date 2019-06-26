8278337afef69d06297d22799da352a7d5495925
Duo Yang <duo.yang@canada.ca>
2019-06-26 22:42:02 +0000

    Add no constrait on SST when nn_ice =5

--- NEMO/OPA_SRC/SBC/sbcmod.F90
+++ NEMO/OPA_SRC/SBC/sbcmod.F90
@@ -297,6 +297,7 @@ CONTAINS
               IF( lk_bdy )   CALL bdy_ice_lim_2( kt )                ! BDY boundary condition
       CASE(  3 )   ;         CALL sbc_ice_lim  ( kt, nsbc )          ! LIM-3 ice model
       CASE(  4 )   ;         CALL sbc_ice_cice ( kt, nsbc )          ! CICE ice model
+      CASE(  5 )   ;         print *, 'nn_ice = 5, no constraint on SST unless restoring'
       END SELECT
       IF (ln_chksum  )   CALL after_state_chksum( "after ice model" )
 
