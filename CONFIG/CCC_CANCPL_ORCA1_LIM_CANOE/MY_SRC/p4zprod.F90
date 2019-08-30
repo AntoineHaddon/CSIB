0775a10b72f5423f721351ece6fa87bb625425c7
Neil Swart <neil.swart@canada.ca>
2019-08-30 23:07:34 +0000

    Revert "Always calculate growth and limitation terms in CanOE runs"
    This reverts commit 4633bc3e178f643f67317e35b0a9ac7b69e7eca0. Further
    debugging of the light limitation needs to happen (canoe-tst03 crashes on April 4)
    To enable the fixes for the optional deck to be merged in, we are reverting the commit
    for now.

--- CONFIG/CCC_CANCPL_ORCA1_LIM_CANOE/MY_SRC/p4zprod.F90
+++ CONFIG/CCC_CANCPL_ORCA1_LIM_CANOE/MY_SRC/p4zprod.F90
@@ -133,93 +133,95 @@ CONTAINS
       DO jk = 1, jpkm1
          DO jj = 1, jpj
             DO ji = 1, jpi
-               
-               ztn = tsn(ji,jj,jk,jp_tem)
-               phyc = MAX(trn(ji,jj,jk,jpphy),0.)*mw_c
-               phyn = MAX(trn(ji,jj,jk,jpnn),0.)*mw_n
-               phyfe = MAX(trn(ji,jj,jk,jpnfe),0.)*mw_fe
-               chl = MAX(trn(ji,jj,jk,jpnch),0.)
-               Ni = MAX(trn(ji,jj,jk,jpno3),0.)
-               Na = MAX(trn(ji,jj,jk,jpnh4),0.)
-               Fe = MAX(trn(ji,jj,jk,jpfer),0.)                        ! Fe variables are in nmol m^-3, others in mmol m^-3
-               ei = etot(ji,jj,jk)*4.15                        ! convert to umol m^-2 s^-1
-               
-               ! this is modified from ~/mexfiles/vrm/fwd/bsource_vrm.f via chemo_2P2Z_gmk.F
-               ! small phytoplankton
-               
-               Tf = tgfuncp(ji,jj,jk)
-               QN = MIN(QNmax1,phyn/(phyc+rtrn))
-               QN = MAX(QNmin1,QN)
-               qndep = MAX((QNmax1-QN)/(QNmax1-QNmin1),0.)                  ! in principle this should be nonegative but if roundoff makes it even slightly negative the exponent could go NaN
-               VCNmax = VCNref*Tf*qndep**0.05
-               Alim  =  Na/(ka1+Na)
-               Nlim  =  Ni/(kn1+Ni)
-               VCN = VCNmax*((1.-Alim)*Nlim+Alim)
-               
-               QFe = MIN(QFemax1,phyfe/(phyc+rtrn))
-               QFe = MAX(QFemin1,QFe)
-               qfedep = MAX((QFemax1-QFe)/(QFemax1-QFemin1),0.) 
-               VCFmax = VCFref*Tf*qfedep**0.05
-               VCF = VCFmax*Fe/(kf1+Fe)
-               
-               PCmax = PCref*Tf*MIN((QFe-QFemin1+rtrn*1.e6)/(QFemax1-QFemin1),(QN-QNmin1+rtrn)/(QNmax1-QNmin1))
-               
-               PCmax = MAX(PCmax,1.0e-10)
-               thetac = MAX(chl/(phyc+rtrn),0.001)
-               PCphot = PCmax*(1.-EXP(-alphachl*ei*thetac/PCmax))
-               rhochl = thetamax*(PCphot/(alphachl*thetac*MAX(ei,0.001)))
-               
-               ! calculate excess intracellular C for exhudation
-               xsphsyn=(phyc/(phyn+rtrn)*mwr_n2c-rr_c2n)*phyn*imw_n
-               xsphsyn=MAX(xsphsyn,0.)
-               
-               zprocn(ji,jj,jk) = (PCphot-eta*VCN)*trn(ji,jj,jk,jpphy)*xstep-kexh*xsphsyn*xstep        ! C production rate (in molar units)
-               zpronn(ji,jj,jk) = VCN/QN*trn(ji,jj,jk,jpnn)*xstep                                      ! N uptake rate
-               zprofen(ji,jj,jk) = VCF/QFe*trn(ji,jj,jk,jpnfe)*xstep                                   ! Fe uptake rate
-               zprochln(ji,jj,jk) = rhochl*VCN/thetac*trn(ji,jj,jk,jpnch)*xstep                        ! Chl production rate
-               zpronew(ji,jj,jk) = zpronn(ji,jj,jk)*(1.-Alim)*Nlim/(Alim+(1.-Alim)*Nlim+rtrn)          ! NO3 uptake
-               xlimnn(ji,jj,jk) = 1.-qndep 
-               xlimnfe(ji,jj,jk) = 1.-qfedep 
-               
-               ! large phytoplankton
-               
-               phyc = MAX(trn(ji,jj,jk,jpdia),0.)*mw_c
-               phyn = MAX(trn(ji,jj,jk,jpdn),0.)*mw_n
-               phyfe = MAX(trn(ji,jj,jk,jpdfe),0.)*mw_fe
-               chl = MAX(trn(ji,jj,jk,jpdch),0.)
-               
-               QN = MIN(QNmax2,phyn/(phyc+rtrn))
-               QN = MAX(QNmin2,QN)
-               qndep = MAX((QNmax2-QN)/(QNmax2-QNmin2),0.) 
-               VCNmax = VCNref*Tf*qndep**0.05
-               Alim  =  Na/(ka2+Na)
-               Nlim  =  Ni/(kn2+Ni)
-               VCN = VCNmax*((1.-Alim)*Nlim+Alim)
-               
-               QFe = MIN(QFemax2,phyfe/(phyc+rtrn))
-               QFe = MAX(QFemin2,QFe)
-               qfedep = MAX((QFemax2-QFe)/(QFemax2-QFemin2),0.) 
-               VCFmax = VCFref*Tf*qfedep**0.05
-               VCF = VCFmax*Fe/(kf2+Fe)
-               
-               PCmax = PCref*Tf*MIN((QFe-QFemin2+rtrn)/(QFemax2-QFemin2),(QN-QNmin2+rtrn)/(QNmax2-QNmin2))
-               
-               PCmax = MAX(PCmax,1.0e-10)
-               thetac = MAX(chl/(phyc+rtrn),0.001)
-               PCphot = PCmax*(1.-EXP(-alphachl*ei*thetac/PCmax))
-               rhochl = thetamax*(PCphot/(alphachl*thetac*MAX(ei,0.001)))
-               
-               xsphsyn=(phyc/(phyn+rtrn)*mwr_n2c-rr_c2n)*phyn*imw_n
-               xsphsyn=MAX(xsphsyn,0.)
-               
-               zprocd(ji,jj,jk) = (PCphot-eta*VCN)*trn(ji,jj,jk,jpdia)*xstep-kexh*xsphsyn*xstep       ! C production rate (in molar units)
-               zprond(ji,jj,jk) = VCN/QN*trn(ji,jj,jk,jpdn)*xstep                                     ! N uptake rate
-               zprofed(ji,jj,jk) = VCF/QFe*trn(ji,jj,jk,jpdfe)*xstep                                  ! Fe uptake rate
-               zprochld(ji,jj,jk) = rhochl*VCN/thetac*trn(ji,jj,jk,jpdch)*xstep                       ! Chl production rate
-               zpronewd(ji,jj,jk) = zprond(ji,jj,jk)*(1.-Alim)*Nlim/(Alim+(1.-Alim)*Nlim+rtrn)        ! NO3 uptake
-               xlimdn(ji,jj,jk) = 1.-qndep 
-               xlimdfe(ji,jj,jk) = 1.-qfedep 
+               IF( etot(ji,jj,jk) > 1.E-3 ) THEN
 
+                      ztn = tsn(ji,jj,jk,jp_tem)
+                      phyc = MAX(trn(ji,jj,jk,jpphy),0.)*mw_c
+                      phyn = MAX(trn(ji,jj,jk,jpnn),0.)*mw_n
+                      phyfe = MAX(trn(ji,jj,jk,jpnfe),0.)*mw_fe
+                      chl = MAX(trn(ji,jj,jk,jpnch),0.)
+                      Ni = MAX(trn(ji,jj,jk,jpno3),0.)
+                      Na = MAX(trn(ji,jj,jk,jpnh4),0.)
+                      Fe = MAX(trn(ji,jj,jk,jpfer),0.)                        ! Fe variables are in nmol m^-3, others in mmol m^-3
+                      ei = etot(ji,jj,jk)*4.15                        ! convert to umol m^-2 s^-1
+
+! this is modified from ~/mexfiles/vrm/fwd/bsource_vrm.f via chemo_2P2Z_gmk.F
+! small phytoplankton
+
+                      Tf = tgfuncp(ji,jj,jk)
+                      QN = MIN(QNmax1,phyn/(phyc+rtrn))
+                      QN = MAX(QNmin1,QN)
+                      qndep = MAX((QNmax1-QN)/(QNmax1-QNmin1),0.)                  ! in principle this should be nonegative but if roundoff makes it even slightly negative the exponent could go NaN
+                      VCNmax = VCNref*Tf*qndep**0.05
+                      Alim  =  Na/(ka1+Na)
+                      Nlim  =  Ni/(kn1+Ni)
+                      VCN = VCNmax*((1.-Alim)*Nlim+Alim)
+
+                      QFe = MIN(QFemax1,phyfe/(phyc+rtrn))
+                      QFe = MAX(QFemin1,QFe)
+                      qfedep = MAX((QFemax1-QFe)/(QFemax1-QFemin1),0.) 
+                      VCFmax = VCFref*Tf*qfedep**0.05
+                      VCF = VCFmax*Fe/(kf1+Fe)
+
+                      PCmax = PCref*Tf*MIN((QFe-QFemin1+rtrn*1.e6)/(QFemax1-QFemin1),(QN-QNmin1+rtrn)/(QNmax1-QNmin1))
+
+                      PCmax = MAX(PCmax,1.0e-10)
+                      thetac = MAX(chl/(phyc+rtrn),0.001)
+                      PCphot = PCmax*(1.-EXP(-alphachl*ei*thetac/PCmax))
+                      rhochl = thetamax*(PCphot/(alphachl*thetac*MAX(ei,0.001)))
+
+! calculate excess intracellular C for exhudation
+                      xsphsyn=(phyc/(phyn+rtrn)*mwr_n2c-rr_c2n)*phyn*imw_n
+                      xsphsyn=MAX(xsphsyn,0.)
+
+                      zprocn(ji,jj,jk) = (PCphot-eta*VCN)*trn(ji,jj,jk,jpphy)*xstep-kexh*xsphsyn*xstep        ! C production rate (in molar units)
+                      zpronn(ji,jj,jk) = VCN/QN*trn(ji,jj,jk,jpnn)*xstep                                      ! N uptake rate
+                      zprofen(ji,jj,jk) = VCF/QFe*trn(ji,jj,jk,jpnfe)*xstep                                   ! Fe uptake rate
+                      zprochln(ji,jj,jk) = rhochl*VCN/thetac*trn(ji,jj,jk,jpnch)*xstep                        ! Chl production rate
+                      zpronew(ji,jj,jk) = zpronn(ji,jj,jk)*(1.-Alim)*Nlim/(Alim+(1.-Alim)*Nlim+rtrn)          ! NO3 uptake
+                      xlimnn(ji,jj,jk) = 1.-qndep 
+                      xlimnfe(ji,jj,jk) = 1.-qfedep 
+
+! large phytoplankton
+
+                      phyc = MAX(trn(ji,jj,jk,jpdia),0.)*mw_c
+                      phyn = MAX(trn(ji,jj,jk,jpdn),0.)*mw_n
+                      phyfe = MAX(trn(ji,jj,jk,jpdfe),0.)*mw_fe
+                      chl = MAX(trn(ji,jj,jk,jpdch),0.)
+
+                      QN = MIN(QNmax2,phyn/(phyc+rtrn))
+                      QN = MAX(QNmin2,QN)
+                      qndep = MAX((QNmax2-QN)/(QNmax2-QNmin2),0.) 
+                      VCNmax = VCNref*Tf*qndep**0.05
+                      Alim  =  Na/(ka2+Na)
+                      Nlim  =  Ni/(kn2+Ni)
+                      VCN = VCNmax*((1.-Alim)*Nlim+Alim)
+
+                      QFe = MIN(QFemax2,phyfe/(phyc+rtrn))
+                      QFe = MAX(QFemin2,QFe)
+                      qfedep = MAX((QFemax2-QFe)/(QFemax2-QFemin2),0.) 
+                      VCFmax = VCFref*Tf*qfedep**0.05
+                      VCF = VCFmax*Fe/(kf2+Fe)
+
+                      PCmax = PCref*Tf*MIN((QFe-QFemin2+rtrn)/(QFemax2-QFemin2),(QN-QNmin2+rtrn)/(QNmax2-QNmin2))
+
+                      PCmax = MAX(PCmax,1.0e-10)
+                      thetac = MAX(chl/(phyc+rtrn),0.001)
+                      PCphot = PCmax*(1.-EXP(-alphachl*ei*thetac/PCmax))
+                      rhochl = thetamax*(PCphot/(alphachl*thetac*MAX(ei,0.001)))
+
+                      xsphsyn=(phyc/(phyn+rtrn)*mwr_n2c-rr_c2n)*phyn*imw_n
+                      xsphsyn=MAX(xsphsyn,0.)
+
+                      zprocd(ji,jj,jk) = (PCphot-eta*VCN)*trn(ji,jj,jk,jpdia)*xstep-kexh*xsphsyn*xstep       ! C production rate (in molar units)
+                      zprond(ji,jj,jk) = VCN/QN*trn(ji,jj,jk,jpdn)*xstep                                     ! N uptake rate
+                      zprofed(ji,jj,jk) = VCF/QFe*trn(ji,jj,jk,jpdfe)*xstep                                  ! Fe uptake rate
+                      zprochld(ji,jj,jk) = rhochl*VCN/thetac*trn(ji,jj,jk,jpdch)*xstep                       ! Chl production rate
+                      zpronewd(ji,jj,jk) = zprond(ji,jj,jk)*(1.-Alim)*Nlim/(Alim+(1.-Alim)*Nlim+rtrn)        ! NO3 uptake
+                      xlimdn(ji,jj,jk) = 1.-qndep 
+                      xlimdfe(ji,jj,jk) = 1.-qfedep 
+
+               ENDIF
             END DO
          END DO
       END DO
