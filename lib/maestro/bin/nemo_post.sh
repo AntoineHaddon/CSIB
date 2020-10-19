523db86a65bc287a9f4e44c3802d6cc5d2786b43
Duo Yang <duo.yang@canada.ca>
2020-10-19 22:46:08 +0000

    Add the icebergs restart to rebuild_list in nemo_post.sh

--- lib/maestro/bin/nemo_post.sh
+++ lib/maestro/bin/nemo_post.sh
@@ -45,6 +45,8 @@ if [ "0" -eq "0" ] ; then
   if [ $pisces_offline -eq 0 ]; then
       rebuild_list="$rebuild_list ${cn_exp}_${end_step}_restart"
       n_rebuild=$(($n_rebuild + 1))
+      rebuild_list="$rebuild_list ${cn_exp}_icebergs_${end_step}_restart"
+      n_rebuild=$(($n_rebuild + 1))
       if [ $nn_ice -eq 2 ]; then
           rebuild_list="$rebuild_list ${cn_exp}_${end_step}_restart_ice"
           n_rebuild=$(($n_rebuild + 1))
