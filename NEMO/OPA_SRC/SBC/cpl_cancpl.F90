20fb627c77077a5b944990cb120b92d52e0c74c6
Clint Seinen <clint.seinen@canada.ca>
2020-11-14 02:01:43 +0000

    Changed name of common coupler param init routine

--- NEMO/OPA_SRC/SBC/cpl_cancpl.F90
+++ NEMO/OPA_SRC/SBC/cpl_cancpl.F90
@@ -336,7 +336,7 @@ contains
      !--- and return an ocean intra-communicator as local_ocn_comm
      !--- NOTE: ocn_master is the rank in MPI_COMM_WORLD not the rank in local_ocn_comm
      local_ocn_comm = -1
-     call init_common_cplr_parameters()
+     call init_common_coupler_parameters()
      call define_group('ocn', local_ocn_comm)
 
      kl_comm  = local_ocn_comm
