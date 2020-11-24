fcea841e1f669600fb8bd0c91e157b6dba1ab8c3
Duo Yang <duo.yang@canada.ca>
2020-11-24 20:01:29 +0000

    Cosmetic changes

--- diag/canesm_nemo_bgc_diag.sh
+++ diag/canesm_nemo_bgc_diag.sh
@@ -15,6 +15,12 @@ set -e
 # NEMO priority level
   output_level=${output_level}
 
+# Note that nemo_rtd_mons used below is first month of the time chunk. 
+# nemo_rtd_mons=1 for a run starting from January in a single 12-month chunk;
+# nemo_rtd_mons=6 for a run starting from June in a single 12-month chunk;
+# nemo_rtd_mons='1 7' for a run starting from January in two 6-month chunks;
+# nemo_rtd_mons='6 12' for a run starting from June in two 6-month chunks.  
+
 # First and last month/year of 12-month period
   fmon=`echo $nemo_rtd_mons | cut -f1 -d' '`
   nmon=`echo $nemo_rtd_mons | wc -w` # number of chunks in 12-month period
