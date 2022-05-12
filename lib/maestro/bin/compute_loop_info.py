b87603b753fdd18ae7d9940f5946594689cee433
Duo Yang <Duo.Yang@ec.gc.ca>
2022-05-12 21:03:33 +0000

    Further revision of compute_loop_info.py to fit Python 3

--- lib/maestro/bin/compute_loop_info.py
+++ lib/maestro/bin/compute_loop_info.py
@@ -138,6 +138,10 @@ if __name__ == '__main__':
         hall = subprocess.check_output(['getdef', 'experiment.cfg', 'hall']).strip()
         nemo_wallclock = subprocess.check_output(['getdef', 'experiment.cfg', 'nemo_wallclock']).strip()
         nprocs = subprocess.check_output(['getdef', 'experiment.cfg', 'tjpnij']).strip()
+        nemo_wallclock = nemo_wallclock.decode()
+        nprocs = nprocs.decode()
+        print ("type of nemo_wallclock", type(nemo_wallclock))
+        print ("type of nprocs", type(nprocs))
 
         # Decode from a byte-like object to string due to Python 2 -> 3
         start = start.decode()
@@ -147,6 +151,11 @@ if __name__ == '__main__':
 
         # Compute how many loops are required in total
         run_start_year, run_start_month, nloops, nemo_freq_months = calc_chunk_nloops(start, end, nemo_freq)
+        nloops = int(nloops)
+        print("type of run_start_year", type(run_start_year))
+        print("type of run_start_month", type(run_start_month))
+        print("type of nloops", type(nloops))
+        print("type of nemo_freq_months", type(nemo_freq_months))
         
         if hall == "hall3":
             frontend='eccc-ppp3'
