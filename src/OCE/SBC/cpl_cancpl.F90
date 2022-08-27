841ca660952cbe9a537911a258d567af7fb2c632
Nicolas Lambert <nicolas.lambert@ec.gc.ca>
2022-08-26 22:17:18 -0400

    Add missing timing switch

--- src/OCE/SBC/cpl_cancpl.F90
+++ src/OCE/SBC/cpl_cancpl.F90
@@ -1026,11 +1026,11 @@ contains
          write(numout,*) '****************'
        endif
 
-       call timing_start('cplsend_gather')
+       IF( ln_timing )   call timing_start('cplsend_gather')
        !--- Gather data into the global array png
       !  call lbc_lnk('cpl_cancpl_snd', pdata(:,:,jc), 'T', 1.)
        call reconstruct_global_2d(pdata(:,:,jc),0,global_array)
-       call timing_stop('cplsend_gather')
+       IF( ln_timing )   call timing_stop('cplsend_gather')
 
        !--- Skip the rest of this loop unless this is the master task
        if ( rank /= ocn_master ) cycle
@@ -1170,13 +1170,13 @@ contains
         !  call copy_1d_to_3d_global(wrk, png)
        endif
 
-       call timing_start('cplrecv_scatter')
+       IF( ln_timing )   call timing_start('cplrecv_scatter')
        !--- Scatter the global array onto each NEMO task
        wrk2d = RESHAPE(wrk,[jpiglo,jpjglo])
        call mppsync
        call mppscatter(wrk2d, 0, pdata(:,:,jc))
        call mppsync
-       call timing_stop('cplrecv_scatter')
+       IF( ln_timing )   call timing_stop('cplrecv_scatter')
 
        if ( rank == ocn_master .and. verbose > 2 ) then
          !--- Count the number of NaNs in the global png array
