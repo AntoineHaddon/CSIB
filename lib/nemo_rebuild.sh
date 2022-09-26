59b3301538539223306788a1ff23ec26ef4cd27f
Nicolas Lambert <nicolas.lambert@ec.gc.ca>
2022-09-26 15:51:21 -0400

    Add some operation to rebuilt the istate files if present (nemo_nn_istate=1)

--- lib/nemo_rebuild.sh
+++ lib/nemo_rebuild.sh
@@ -151,6 +151,8 @@ if [ -s "$indir" ] ; then
         # Replace the global lat/lon to remove the hold made by the land processors elimination
    ncks -x -h -O -v  nav_lon,nav_lat,glamf,gphif,glamv,gphiv,glamu,gphiu,glamt,gphit ${pfx}.nc ${pfx}.nc 
    ncks -A -h -v nav_lon,nav_lat,glamf,gphif,glamv,gphiv,glamu,gphiu,glamt,gphit  ${wrkdir}/coor.nc ${pfx}.nc  
+   ncks -x -h -O -v  e1f,e2f,e1v,e2v,e1u,e2u,e1t,e2t ${pfx}.nc ${pfx}.nc 
+   ncks -A -h -v e1f,e2f,e1v,e2v,e1u,e2u,e1t,e2t  ${wrkdir}/coor.nc ${pfx}.nc  
    save ${pfx}.nc $ncsave
 
    # cleanup
@@ -181,6 +183,18 @@ cd $inrs
 ls -l  rs_time.step
 nn_itend=$(cat rs_time.step)
 end_step=$(echo $nn_itend | awk '{printf "%8.8d",$1}')
+
+# The initial state files
+pfx=output.init
+# Check if the RS is already rebuilt, in which case do nothing.
+if [ -s "${pfx}_0000.nc" ]; then
+   rebuild_nemo_tiles
+   # Replace the global lat/lon to remove the hold made by the land processors elimination
+   ncks -x -h -O -v  nav_lon,nav_lat $pfx.nc $pfx.nc
+   ncks -A -h -v nav_lon,nav_lat ${wrkdir}/coor.nc $pfx.nc
+   ncsave=${model1}_istate_$start_date.nc
+   mv  $pfx.nc $ncsave
+fi
 
 # The physics rs file
 pfx=${runid}_${end_step}_restart
