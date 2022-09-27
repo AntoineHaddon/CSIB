e21858df87f645aa9ae759d4a3a59e18c770a6e0
Jim Christian <jim.christian@ec.gc.ca>
2022-09-27 18:48:05 +0000

    overwrote base case nemo4-integration files with the ones from Olivier's nemo4-bgcm_suite_u2-or_dvlp branch, for TOP root directory only (subsidiary directories to come later)

--- nemo/src/TOP/trcsms.F90
+++ nemo/src/TOP/trcsms.F90
@@ -14,6 +14,9 @@ MODULE trcsms
    !!----------------------------------------------------------------------
    USE oce_trc            !
    USE trc                !
+
+   USE trcsms_canoe       ! CANOE  biogeo-model
+   USE trcsms_cmoc        ! CMOC   biogeo-model
    USE trcsms_pisces      ! PISCES biogeo-model
    USE trcsms_cfc         ! CFC 11 &/or 12
    USE trcsms_c14         ! C14 
@@ -21,10 +24,19 @@ MODULE trcsms
    USE trcsms_my_trc      ! MY_TRC  tracers
    USE prtctl_trc         ! Print control for debbuging
 
+   ! TOP-level processes time integration
+   USE par_trc            ! TOP parameters
+   USE trd_oce			      ! Ocean trends :   set tracer and momentum trend variables	
+                          ! contains logical switches and jp_*** style tracer/var. indices
+   USE trdtrc			        ! Dummy module??? O Riche July 5th 2022 not sure what's that doing.
+
+   USE sms_top
+
    IMPLICIT NONE
    PRIVATE
 
-   PUBLIC   trc_sms    ! called in trcstp.F90
+   PUBLIC   trc_sms       ! called in trcstp.F90
+   PUBLIC   trc_sms_alloc ! called by trcini.F90?
 
    !!----------------------------------------------------------------------
    !! NEMO/TOP 4.0 , NEMO Consortium (2018)
@@ -41,28 +53,95 @@ CONTAINS
       !!
       !! ** Method  : -  call the main routine of of each defined tracer model
       !! -------------------------------------------------------------------------------------
-      INTEGER, INTENT( in ) ::   kt      ! ocean time-step index      
       !!
+      !!
+      INTEGER, INTENT( in ) ::   kt      ! ocean time-step index
       CHARACTER (len=25) :: charout
       !!---------------------------------------------------------------------
       !
       IF( ln_timing )   CALL timing_start('trc_sms')
+
       !
+      IF(lwp) WRITE(numout,*)
+      IF(lwp) WRITE(numout,*) ' trc_sms:  shared BGC processes'
+      IF(lwp) WRITE(numout,*) ' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+      !
+      !
+      ! O Riche July 05th 2022
+      ! undo this as conditional branching
+      ! requires up-to-date kt time step.
+      ! perhaps move it higher up
+      ! in trcstp.F90. Will it be consistent
+      ! with restart state? It won't work
+      ! necessarily as rfact is used in other
+      ! part of TOP, e.g. rfact2 in TRP/trcnxt.F90
+      ! it is possible that var. scope will
+      ! locally prevent any conflict with global
+      ! variables 
+      ! O Riche July 05th 2022
+      ! moved from trcsms.F90 to here
+      ! to be consistent with its location
+      ! in CanESM/CanNEMO code and the migration
+      ! of common code to TOP-tier modules
+      ! O Riche June 13th 2022
+      ! Time step header, select the namelist time step
+      ! or double it if Leap-Frog and not Euler has been selected as
+      ! the time integration scheme
+      !
+      rfact = r2dttrc
+      ! O Riche July 5th 2022
+      ! mitigating the impact of this line for now but might want to keep it
+      ! or upgrade it in the final version of the code.
+      ! nrdttrc enables biology components of BGCMs to integrate over extra shorter time steps.
+      ! not to confuse with nn_dttrc (subdivide physics time steps to drive BGCMs) and rdttrc
+      ! the new time step for BGCM tracers if nn_dtrc/=1
+      nrdttrc = 4 ! should be read from namelist_pisces (or _canoe) by trcnam_pisces (or _canoe) or perhaps moved to namelist_top
+      !
+      IF( ( ln_top_euler .AND. kt == nittrc000 )  .OR. ( .NOT.ln_top_euler .AND. kt <= nittrc000 + nn_dttrc ) ) THEN
+        rfactr  = 1. / rfact
+        rfact2  = rfact / REAL( nrdttrc, wp )
+        rfact2r = 1. / rfact2
+  
+        IF(lwp) WRITE(numout,*) 
+        IF(lwp) WRITE(numout,*) '    Passive Tracer  time step    rfact  = ', rfact, ' rdt = ', rdt
+        IF(lwp) write(numout,*) '            Biology time step    rfact2 = ', rfact2
+        IF(lwp) WRITE(numout,*)
+
+      ENDIF
+      !
+      !
+      !
+      IF( ln_canoe   )   CALL trc_sms_canoe  ( kt )    ! main program of CANOE  
+      IF( ln_cmoc    )   CALL trc_sms_cmoc   ( kt )    ! main program of CMOC   
       IF( ln_pisces  )   CALL trc_sms_pisces ( kt )    ! main program of PISCES 
       IF( ll_cfc     )   CALL trc_sms_cfc    ( kt )    ! surface fluxes of CFC
       IF( ln_c14     )   CALL trc_sms_c14    ( kt )    ! surface fluxes of C14
       IF( ln_age     )   CALL trc_sms_age    ( kt )    ! Age tracer
       IF( ln_my_trc  )   CALL trc_sms_my_trc ( kt )    ! MY_TRC  tracers
-
+  
       IF(ln_ctl) THEN      ! print mean trends (used for debugging)
-         WRITE(charout, FMT="('sms ')")
-         CALL prt_ctl_trc_info( charout )
-         CALL prt_ctl_trc( tab4d=trn, mask=tmask, clinfo=ctrcnm )
+        WRITE(charout, FMT="('sms ')")
+        CALL prt_ctl_trc_info( charout )
+        CALL prt_ctl_trc( tab4d=trn, mask=tmask, clinfo=ctrcnm )
       ENDIF
       !
       IF( ln_timing )   CALL timing_stop('trc_sms')
       !
    END SUBROUTINE trc_sms
+
+
+   INTEGER FUNCTION trc_sms_alloc()
+      !!----------------------------------------------------------------------
+      !!              ***  ROUTINE trc_sms_alloc  ***
+      !!----------------------------------------------------------------------
+      !
+      ! ALLOCATE here the arrays specific to TOP
+      ! ALLOCATE( tab(...) , STAT=trc_sms_alloc )
+      trc_sms_alloc = 0      ! set to zero if no array to be allocated
+      !
+      IF( trc_sms_alloc /= 0 ) CALL ctl_stop( 'STOP', 'trc_sms_alloc : failed to allocate arrays' )
+      !
+   END FUNCTION trc_sms_alloc
 
 #else
    !!======================================================================
