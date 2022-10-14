03be141121b8973be13ac7209441a81f0511cf1b
Olivier Riche <Olivier.Riche@ec.gc.ca>
2022-10-14 13:28:14 -0400

    Add prefix q- to rfact* variables.

--- src/TOP/CANBGC/sms_top_canbgc.F90
+++ src/TOP/CANBGC/sms_top_canbgc.F90
@@ -20,8 +20,8 @@ MODULE sms_top_canbgc
 	!!*  Time variables
 	INTEGER  ::   qnrdttrc           !: BGCM time-step multiplier, i.e. ocean time step * qnrdttrc = BGCM time step
 	INTEGER  ::   qndayflxtr         !: use to check for new day when updating carbon chemistry state in CANOE    
-	REAL(wp) ::   qfact , qfactr    !: BGCM time-step rfact, and its inverse, both used if Euler scheme is in use
-	REAL(wp) ::   qfact2, qfact2r   !: BGCM time-step rfact2, and inverse,    both used if Leap-Frog scheme is in use   
+	REAL(wp) ::   qfact , qfactr    !: BGCM time-step qrfact, and its inverse, both used if Euler scheme is in use
+	REAL(wp) ::   qfact2, qfact2r   !: BGCM time-step qrfact2, and inverse,    both used if Leap-Frog scheme is in use   
 
   !!* Mass conservation
   ! LOGICAL  ::  ln_check_mass_canoe  !: Flag to check mass conservation
@@ -82,9 +82,6 @@ MODULE sms_top_canbgc
 
 
    !!*  Time variables
-   ! INTEGER  ::   nrdttrc           !: ???
-   ! REAL(wp) ::   rfact , rfactr    !: ???
-   ! REAL(wp) ::   rfact2, rfact2r   !: ???
    REAL(wp) ::   xstepb             !: Time step duration for biology
    REAL(wp) ::   ryyssb             !: number of seconds per year 
    REAL(wp) ::   r1_ryyssb          !: inverse number of seconds per year 
