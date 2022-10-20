7cf275689ea00189f900c4912322684a0d5caa3f
Olivier Riche <Olivier.Riche@ec.gc.ca>
2022-10-20 12:32:31 -0400

    Fix a typo in the marker zetot.

--- src/TOP/CANBGC/CMOC/cmocprod.F90
+++ src/TOP/CANBGC/CMOC/cmocprod.F90
@@ -194,7 +194,7 @@ CONTAINS
       END DO
       ! OR Oct 19th 2022 DBG1
       IF( lwp ) THEN
-        WRITE(numout,*) '    zeto(100,100,1) = ',     zeto(100,100,1)
+        WRITE(numout,*) '   zetot(100,100,1) = ',    zetot(100,100,1)
         WRITE(numout,*) '  zprbio(100,100,1) = ',   zprbio(100,100,1)
         WRITE(numout,*) ' zprorca(100,100,1) = ',  zprorca(100,100,1)
         WRITE(numout,*) 'zprochln(100,100,1) = ', zprochln(100,100,1)
