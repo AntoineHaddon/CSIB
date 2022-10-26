997ef169c329d5d4269076b37fd84edd6e26f1b2
Olivier Riche <Olivier.Riche@ec.gc.ca>
2022-10-26 14:54:08 -0400

    Reinstate jqnch in cmocprod.F90.

--- nemo/src/TOP/CANBGC/CMOC/cmocprod.F90
+++ nemo/src/TOP/CANBGC/CMOC/cmocprod.F90
@@ -189,7 +189,7 @@ CONTAINS
             !
             tra(ji,jj,jk,jqno3) = tra(ji,jj,jk,jqno3) - zprorca(ji,jj,jk)
             ! tra(ji,jj,jk,jqphy) = tra(ji,jj,jk,jqphy) + zprorca(ji,jj,jk)
-            ! tra(ji,jj,jk,jqnch) = tra(ji,jj,jk,jqnch) + zprochln(ji,jj,jk)
+            tra(ji,jj,jk,jqnch) = tra(ji,jj,jk,jqnch) + zprochln(ji,jj,jk)
             tra(ji,jj,jk,jqoxy) = tra(ji,jj,jk,jqoxy) + zprorca(ji,jj,jk)
             tra(ji,jj,jk,jqdic) = tra(ji,jj,jk,jqdic) - zprorca(ji,jj,jk)
             tra(ji,jj,jk,jqtal) = tra(ji,jj,jk,jqtal) + ncrr_cmoc * zprorca(ji,jj,jk)
