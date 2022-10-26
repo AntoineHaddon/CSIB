62ecd9e076bff382fed3b357b1e8752dd3151db4
Olivier Riche <Olivier.Riche@ec.gc.ca>
2022-10-26 15:31:10 -0400

    Some tracers are depleted in this module (no3/dic), I am doing a test without them, meaning I am reinstating jpqnch and jqphy.

--- nemo/src/TOP/CANBGC/CMOC/cmocprod.F90
+++ nemo/src/TOP/CANBGC/CMOC/cmocprod.F90
@@ -187,11 +187,11 @@ CONTAINS
          DO jj = 1, jpj
            DO ji =1 ,jpi
             !
-            tra(ji,jj,jk,jqno3) = tra(ji,jj,jk,jqno3) - zprorca(ji,jj,jk)
-            ! tra(ji,jj,jk,jqphy) = tra(ji,jj,jk,jqphy) + zprorca(ji,jj,jk)
+            ! tra(ji,jj,jk,jqno3) = tra(ji,jj,jk,jqno3) - zprorca(ji,jj,jk)
+            tra(ji,jj,jk,jqphy) = tra(ji,jj,jk,jqphy) + zprorca(ji,jj,jk)
             tra(ji,jj,jk,jqnch) = tra(ji,jj,jk,jqnch) + zprochln(ji,jj,jk)
             tra(ji,jj,jk,jqoxy) = tra(ji,jj,jk,jqoxy) + zprorca(ji,jj,jk)
-            tra(ji,jj,jk,jqdic) = tra(ji,jj,jk,jqdic) - zprorca(ji,jj,jk)
+            ! tra(ji,jj,jk,jqdic) = tra(ji,jj,jk,jqdic) - zprorca(ji,jj,jk)
             tra(ji,jj,jk,jqtal) = tra(ji,jj,jk,jqtal) + ncrr_cmoc * zprorca(ji,jj,jk)
             ! O Riche Sept 14th can be uncommented or moved to TOP
             ! tra(ji,jj,jk,jqdnt) = tra(ji,jj,jk,jqdnt) - zprorca(ji,jj,jk)
