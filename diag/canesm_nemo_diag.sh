4c281ddcebe798590738ead2492ffb009be759ee
Nicolas Lambert <nicolas.lambert@ec.gc.ca>
2023-07-14 15:34:32 -0400

    Remove the duplicate of cdo splitname.

--- diag/canesm_nemo_diag.sh
+++ diag/canesm_nemo_diag.sh
@@ -63,9 +63,6 @@ set -x
   for sfx in $nemo_diag_file_suffix_list ; do
     diag_hist="mc_${runid}_${fyear}_m${fmon}_${sfx}.nc"
     access ${sfx}_${fmon} $diag_hist na
-    [ ! -e ${sfx}_${fmon} ] && continue
-    ncks -O -C -x -v time_centered_bounds,time_centered ${sfx}_${fmon} ${sfx}_${fmon} 
-    cdo splitname ${sfx}_${fmon} xxx-${sfx}_
   done
 
 # Execute the following lines when output_level -ge 1
