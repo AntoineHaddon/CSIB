b5e1787922e2895bea609a302940c1a2debb3d73
Neil Swart <neil.swart@ec.gc.ca>
2023-09-16 00:45:11 +0000

    Remove unused crud from NEMO rtd script

--- diag/canesm_nemo_runtime_diag.sh
+++ diag/canesm_nemo_runtime_diag.sh
@@ -4,18 +4,6 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   stamp=$(date "+%j%H%M%S"$$)
 
-  # this_host will simply be the output from uname
-  this_host=$(uname -n|awk -F. '{print $1}' -)
-
-  # this_mach will be a known alias (or possibly the actual machine name)
-  this_mach=$this_host
-  ROUTE_SSH=''
-  case $this_mach in
-             xc3*) this_mach=banting; ROUTE_SSH='ssh banting';;
-             xc4*) this_mach=daley;   ROUTE_SSH='ssh daley'  ;;
-      hpcr3*|cs3*|ppp3*|eccc*-ppp3*) this_mach=ppp3;   ROUTE_SSH='ssh ppp3'  ;;
-      hpcr4*|cs4*|ppp4*|eccc*-ppp4*) this_mach=ppp4;   ROUTE_SSH='ssh ppp4'  ;;
-  esac
 
   function rtd_staging() {
     # Copy a single rtd file to a local staging directory
@@ -34,21 +22,9 @@
 
   # bail is a simple error exit routine
   bail_prefix="NEMO RTD"
-  bail(){
+  bail(){``
     echo `date`" $runid --- ${bail_prefix}: $*"
     exit 1
-  }
-
-  # function to extract files types from plot yaml files
-  get_plot_filetypes() {
-    plt_fts=`cat $1 | grep 'ifile' | awk '{print $2}'`
-    fts=""
-    for ft in $plt_fts; do
-      if [[ ${fts} != *${ft}* ]]; then
-        fts=${fts}" "${ft}
-      fi
-    done
-    echo $fts
   }
 
 #~~~~~~~~~~~~~~~~~~~
@@ -189,11 +165,6 @@
    # Cleanup
    release orca_mesh_mask
 
-#   Modify attributes
-    [ -s nemo_physical_rtd.nc ] && ncatted -a RTD_version,global,o,c,"${physical_rtd_exe}\n" nemo_physical_rtd.nc
-    [ -s nemo_ice_rtd.nc ] && ncatted  -a RTD_version,global,o,c,"${ice_rtd_exe}\n" nemo_ice_rtd.nc
-    [ -s nemo_carbon_rtd.nc ] && ncatted -a RTD_version,global,o,c,"${carbon_rtd_exe}\n" nemo_carbon_rtd.nc
-
 #   Move RTD files for staging back to Victoria
     rtd_staging nemo_physical_rtd.nc $physical_rtdfile
     rtd_staging nemo_ice_rtd.nc      $ice_rtdfile
@@ -215,202 +186,3 @@
       delete oldi na
       delete oldc na
     fi   
-
-############# NemoVal diagnostics #############
-    if [ $with_nemoval_diag -eq 1 ] ; then
-        
-		# determine if plotting/merging should be done
-		nemoval_mod=$((year % nemoval_repeat_period)) 
-		noplt_period=$((nemoval_repeat_period - nemoval_avg_period)) 
-		if [ $nemoval_mod -eq 0 ] || [ $nemoval_mod -gt $noplt_period ]; then
-        	export SKIP_SAME_TIME=1
-
-			# copy the NView settings files and source
-			cp ${nemoval_root}/data/NViewConfig.sh .
-			. ./NViewConfig.sh
-
-			# determine plot years
-			if [ $nemoval_mod -eq 0 ]; then
-				pltey=$year
-			else
-				nval_tmp=$((year + (nemoval_repeat_period - nemoval_mod)))
-				pltey=$(printf "%04d" $nval_tmp)
-			fi
-			# note: the + 1 is required since we want plots to INCLUDE the final year
-			pltsy=$(printf "%04d" $((pltey - nemoval_avg_period + 1))) 
-
-			# make sure data should actually exists by confirming the run start year 
-			#    is less than the plot start year
-			rsy=$year_rtdiag_start
-
-            # output useful messages that will help for debugging
-            echo "Trying to performing nemoval file handling for a plot for years ${pltey} to ${pltsy}"
-            echo "Simulation at year ${year} and started at ${rsy}"
-			if [ $pltsy -ge $rsy ]; then
-                echo "In a valid nemoval plotting period"
-           
-				# get previous end year 
-			  	prey=$(printf "%04d" $((year - 1)))
-
-			  	# set tmp merged file suffix
-			  	new_int_mrg_file_suf="model_rtime_nval_tmp_${runid}_${pltsy}_${year}_"
-			  	prev_int_mrg_file_suf="model_rtime_nval_tmp_${runid}_${pltsy}_${prey}_"
-
-                # get filetypes
-                filetypes=`get_plot_filetypes $nemoval_pltyml`
-
-                # check if this is a plot year
-			  	if [ $year -eq $pltey ]; then
-                    echo "Performing final file handling and launching plot job to front end machine"
-
-            		# get template for nemoval job
-            		cp ${nemoval_root}/data/model_rtime_files/model_rtime_template .
-
-            		# perform final .nc handling, time average, and store in nemoval_root
-            		for ft in $filetypes; do
-                        # make filetype lowercase to match cpld naming convention
-                        ft=$(echo $ft | tr '[:upper:]' '[:lower:]')
-
-                        # determine year's input files and access them
-                        yr_fls=""
-                        for mon in $nemo_rtd_mons; do
-                            fl="mc_${runid}_${year}_m${mon}_1m_${ft}.nc"
-                            access $fl $fl
-                            yr_fls=${yr_fls}${fl}" "
-                        done
-
-                        if [ $year -eq $pltsy ]; then
-                            # then no merged file has been created yet and plots are just for this year
-                            
-                            # merge all year's files into one
-                            outpt_fl=model_rtime_nval_tmp_${runid}_${pltsy}_${pltey}_${ft}.nc
-                            ${CDO_PTH}/cdo mergetime $yr_fls $outpt_fl
-
-                            # clean up
-                            release $yr_fls
-                        else
-                            # perform final merger
-
-                            # access previously merged file
-                            inpt_fl="${prev_int_mrg_file_suf}${ft}.nc"
-                            access $inpt_fl $inpt_fl
-
-                            # merge
-                            outpt_fl="${new_int_mrg_file_suf}${ft}.nc"
-                            ${CDO_PTH}/cdo mergetime $inpt_fl $yr_fls $outpt_fl
-
-                            # clean up
-                            delete $inpt_fl    # delete previous merge file
-                            release $yr_fls     # release files from this year
-                        fi
-
-                        # time average
-                        inpt_fl="$outpt_fl"
-                        outpt_fl="${MRG_DATA_DIR}/mc_${runid}_1m_${pltsy}0101_${pltey}1231_${ft}.nc"
-                        ${CDO_PTH}/cdo timmean $inpt_fl $outpt_fl
-                        rm $inpt_fl
-                    done
-
-                    #---- setup/launch plotting job 
-                    # replace name of python environment with its absolute path
-                    PY_ENV="/home/$SCI_ID/.conda/envs/$PY_ENV"
-                    sed -i "s#PY_ENV=.*#PY_ENV=\"$PY_ENV\"#g" NViewConfig.sh
-
-                    # replace activate exectubale with its full/absolute path
-                    ACTV_EXE="$PY_ENV/bin/activate"
-                    sed -i "s#ACTV_EXE=.*#ACTV_EXE=\"$ACTV_EXE\"#g" NViewConfig.sh
-                        
-                    # replace SCI_ID with user running the model
-                    SCI_ID=$(whoami)
-                    sed -i "s#SCI_ID=.*#SCI_ID=\"$SCI_ID\"#g" NViewConfig.sh
-
-                    # set plotting machine
-                    RUN_MACH=`echo $HDNODE | sed 's/eccc-//'`
-
-                    # set mask to be used by the plotting scripts
-                    if [ $nemoval_res == "2d" ]; then
-                        MASK="orca2_tmask_miss.nc"
-                    elif [ $nemoval_res == "1d" ]; then
-                        MASK="orca1_tmask_miss.nc"
-                    elif [ $nemoval_res == "025d" ]; then
-                        MASK="orca025_tmask_miss.nc"
-                    else
-                        bail "you selected a resolution not supported by NemoVal plots..either decrease resolution or turn off NemoVal plots"
-                    fi
-                
-                    # get unique PID/DATE string to ID the plotting job 
-                    plt_ID=$$_`date +%Y-%m-%d%H%M%S`
-
-                    # produce job script
-                    cat model_rtime_template | sed "s#RUNID#$runid#g;s#START#$pltsy#g;\
-                    s#END#$pltey#g;s#SCI_ID#$SCI_ID#g;s#NV_USER#canesm_nemoval#g;s#NVAL_ROOT#$nemoval_root#g;\
-                    s#PID#$plt_ID#g;s#RUN_MACH#$RUN_MACH#g;s#VERSION#$DEF_NVAL_VER#g;\
-                    s#OBS_DIR#${nemoval_root}/data/${DEF_OBS_DIR}#g;s#MASKFILE#$MASK#g;s#PLTYML_PATH/PLOTS#$nemoval_pltyml#g;\
-                    s#PLOTS#`basename $nemoval_pltyml`#g;s#OBSERVED#$DEF_OBS_LST#g;s#PY_ENV#$PY_ENV#g;s#ACTV_EXE#$ACTV_EXE#g;\
-                    s#FILETYPES#$filetypes#g"\
-                    >> canesm_nvaljob_${runid}_${pltsy}_${pltey}
-
-                    # remove useless template and place NViewConfig.sh in NemoVal's staging directory
-                    if [ $? -eq 0 ]; then
-                        mv NViewConfig.sh ${nemoval_root}/stg/NViewConfig_${plt_ID}_.sh
-                        rm model_rtime_template
-                    fi
-                    
-                    # submit job
-                    rsub $RUN_MACH canesm_nvaljob_${runid}_${pltsy}_${pltey} > /dev/null 2>&1
-                    if [ $? -eq 0 ]; then
-                        echo "NemoVal plotting job submitted"
-                    fi
-                elif [ $year -eq $pltsy ]; then
-                    echo "Creating new merged file"
-                    
-                    for ft in $filetypes; do
-                        # translate ft to lower case
-                        ft=$(echo $ft | tr '[:upper:]' '[:lower:]')
-
-                        # determine year's input files and access them
-                        yr_fls=""
-                        for mon in $nemo_rtd_mons; do
-                            fl="mc_${runid}_${year}_m${mon}_1m_${ft}.nc"
-                            access $fl $fl
-                            yr_fls=${yr_fls}${fl}" "
-                        done
-
-                        # merge year's files into one
-                        outpt_fl="${new_int_mrg_file_suf}${ft}.nc"
-                        ${CDO_PTH}/cdo mergetime $yr_fls $outpt_fl
-
-                        # clean up
-                        release $yr_fls
-                        save $outpt_fl $outpt_fl
-                    done
-                else
-                    echo "Appending to existing merged file"
-                    for ft in $filetypes; do
-                        # translate ft to lower case
-                        ft=$(echo $ft | tr '[:upper:]' '[:lower:]')
-
-                        # determine year's input files and access them
-                        yr_fls=""
-                        for mon in $nemo_rtd_mons; do
-                            fl="mc_${runid}_${year}_m${mon}_1m_${ft}.nc"
-                            access $fl $fl
-                            yr_fls=${yr_fls}${fl}" "
-                        done
-
-                        # set files
-                        inpt_fl="${prev_int_mrg_file_suf}${ft}.nc" 
-                        access $inpt_fl $inpt_fl
-                        outpt_fl="${new_int_mrg_file_suf}${ft}.nc"
-
-                        ${CDO_PTH}/cdo mergetime $inpt_fl $yr_fls $outpt_fl
-
-                        # clean up
-                        release $yr_fls
-                        delete $inpt_fl
-                        save $outpt_fl $outpt_fl; rm $outpt_fl
-                    done
-                fi
-            fi
-        fi
-    fi
