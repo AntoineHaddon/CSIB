ca5842c5b69fe082ec66945610dc8df90e77be0f
Duo Yang <Duo.Yang@ec.gc.ca>
2024-01-20 00:44:49 +0000

    Bug-fix in canesm_nemo_diag.sh

--- diag/canesm_nemo_diag.sh
+++ diag/canesm_nemo_diag.sh
@@ -92,7 +92,8 @@ set -e
       # output_level=1 and only if starting from January
       1)
         # access input variables for computing vars with priority level 1
-        if [ $fmon -eq 1 && $nemo_calc_diag -eq 1 ] ; then
+        
+        if [[ $fmon -eq 1 && $nemo_calc_diag == 1 ]] ; then
           ln -sf 1m_grid_t_${fmon} grid_t  || bail "Link to grid_t failed"
           ln -sf 1m_grid_u_${fmon} grid_u  || bail "Link to grid_u failed"
           ln -sf 1m_grid_v_${fmon} grid_v  || bail "Link to grid_v failed"
