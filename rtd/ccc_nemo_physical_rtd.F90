3800a84d78d7ea1c2a9f3dcc439831994c8b96a7
Nicolas Lambert <nicolas.lambert@ec.gc.ca>
2024-05-01 13:45:03 -0400

    Correction on the detection of the nemo_runoff variable for fetching the input files.

--- rtd/ccc_nemo_physical_rtd.F90
+++ rtd/ccc_nemo_physical_rtd.F90
@@ -500,7 +500,7 @@ PROGRAM nemo_ocean_diag
           CALL getvara ('sndmasssnf', iou1, imt*jmt, (/1,1,l/), (/imt,jmt,1/), snow_ai_cea, 1., 0.)
           CALL getvara ('prsn', iou1, imt*jmt, (/1,1,l/), (/imt,jmt,1/), snow_ao_cea, 1., 0.)
           CALL getvara ('hfrainds', iou1, imt*jmt, (/1,1,l/), (/imt,jmt,1/), hflx_rain_cea, 1., 0.)
-          CALL getvara ('hfsnthermds', iou1, imt*jmt, (/1,1,l/), (/imt,jmt,1/), hflx_snow_ao_cea, 1., 0.)
+          CALL getvara ('hflx_snow_ao_cea', iou1, imt*jmt, (/1,1,l/), (/imt,jmt,1/), hflx_snow_ao_cea, 1., 0.)
           CALL getvara ('hflx_snow_ai_cea', iou6, imt*jmt, (/1,1,l/), (/imt,jmt,1/), hflx_snow_ai_cea, 1., 0.)
           CALL getvara ('hfxsensib', iou6, imt*jmt, (/1,1,l/), (/imt,jmt,1/), hflx_ice_cea, 1., 0.)
           CALL getvara ('hfrunoffds2d', iou1, imt*jmt, (/1,1,l/), (/imt,jmt,1/), hflx_rnf_cea, 1., 0.)
