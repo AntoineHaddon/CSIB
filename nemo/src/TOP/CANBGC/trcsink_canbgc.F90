ab5d2fbd6ca0bac80e6b7d353054f51d1d847bc3
Elise Olson <Elise.Olson@ec.gc.ca>
2024-12-01 02:28:49 +0000

    Try again, add write

--- nemo/src/TOP/CANBGC/trcsink_canbgc.F90
+++ nemo/src/TOP/CANBGC/trcsink_canbgc.F90
@@ -465,7 +465,8 @@ CONTAINS
             ikt = mbkt(ji,jj)
             zcalbotflx = tr(ji,jj,ikt,jrcal,Kbb) * wscal(ji,jj,ikt) * xstepb
             zfactcal=0.
-            IF( qomegac(ji,jj,ikt)>1. ) zfactcal=1.
+            WRITE(numout,*) "ji,jj,ikt,qomegac:", ji, jj, ikt, qomegac(ji,jj,ikt)
+            IF( ikt>0 .AND. qomegac(ji,jj,ikt)>1. ) zfactcal=1.
             !zfactcal = FLOAT(FLOOR(MIN( qomegac(ji,jj,ikt), 1.5 )))       ! set burial fraction to 1 if Omega>1 and 0 otherwise
             tr(ji,jj,ikt,jrcal, Krhs) = tr(ji,jj,ikt,jrcal, Krhs) - zcalbotflx / e3t(ji,jj,ikt, Kmm)
             ! add DIC/TA to bottom layer if dissolution and to surface layer if burial
