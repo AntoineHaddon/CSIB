33fcdaf2af3474aae527f0d67468d7fb0f37356b
Elise Olson <Elise.Olson@ec.gc.ca>
2024-12-01 22:50:20 +0000

    Do not write out chl src; it is not used

--- nemo/src/TOP/CANBGC/CANOE/trcwri_canoe.F90
+++ nemo/src/TOP/CANBGC/CANOE/trcwri_canoe.F90
@@ -50,7 +50,7 @@ CONTAINS
       END DO
       !
       ! Testing trcopt diagnostics
-      CALL iom_put( "surf_chla", src2d_dta(:,:,js2d_chla))
+      !CALL iom_put( "surf_chla", src2d_dta(:,:,js2d_chla))
       !CALL iom_put( "tmask", tmask(:,:,:) )
       CALL iom_put( "closea", tmask_bgc_closea(:,:,:) )
       !
