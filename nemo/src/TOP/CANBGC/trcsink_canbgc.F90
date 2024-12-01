3fe5b6d61083ab4b7e31086c0e69bb431c29befe
Elise Olson <Elise.Olson@ec.gc.ca>
2024-12-01 02:57:22 +0000

    Turn off write and attempt to avoid large values in masked cells in qomegac

--- nemo/src/TOP/CANBGC/trcsink_canbgc.F90
+++ nemo/src/TOP/CANBGC/trcsink_canbgc.F90
@@ -465,7 +465,7 @@ CONTAINS
             ikt = mbkt(ji,jj)
             zcalbotflx = tr(ji,jj,ikt,jrcal,Kbb) * wscal(ji,jj,ikt) * xstepb
             zfactcal=0.
-            WRITE(numout,*) "ji,jj,ikt,qomegac:", ji, jj, ikt, qomegac(ji,jj,ikt)
+            !WRITE(numout,*) "ji,jj,ikt,qomegac:", ji, jj, ikt, qomegac(ji,jj,ikt)
             IF( ikt>0 .AND. qomegac(ji,jj,ikt)>1. ) zfactcal=1.
             !zfactcal = FLOAT(FLOOR(MIN( qomegac(ji,jj,ikt), 1.5 )))       ! set burial fraction to 1 if Omega>1 and 0 otherwise
             tr(ji,jj,ikt,jrcal, Krhs) = tr(ji,jj,ikt,jrcal, Krhs) - zcalbotflx / e3t(ji,jj,ikt, Kmm)
